f8bde69359b7658cfcb2c7b691257d930bb03957
jeonghoon <shb03323@naver.com>
2023-07-18 13:09:25 +0900

    feat: Company null 예외 검증 구현

--- backend/baton/src/main/java/touch/baton/domain/member/vo/Company.java
+++ backend/baton/src/main/java/touch/baton/domain/member/vo/Company.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Embeddable;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 import static lombok.AccessLevel.PROTECTED;
 
 @Getter
@@ -12,10 +14,17 @@ import static lombok.AccessLevel.PROTECTED;
 @Embeddable
 public class Company {
 
-    @Column(name = "company")
+    @Column(name = "company", nullable = false)
     private String value;
 
     public Company(final String value) {
+        validateNotNull(value);
         this.value = value;
+    }
+
+    private void validateNotNull(final String value) {
+        if (Objects.isNull(value)) {
+            throw new IllegalArgumentException("company 는 null 일 수 없습니다.");
+        }
     }
 }
