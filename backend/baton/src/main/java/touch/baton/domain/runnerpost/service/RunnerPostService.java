f20071e7e936d6d586beeaf81ca2468aca76b97c
jeonghoon <shb03323@naver.com>
2023-07-18 23:39:37 +0900

    feat: service 레이어 구현

--- backend/baton/src/main/java/touch/baton/domain/runnerpost/service/RunnerPostService.java
+++ backend/baton/src/main/java/touch/baton/domain/runnerpost/service/RunnerPostService.java
@@ -3,7 +3,13 @@ package touch.baton.domain.runnerpost.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import touch.baton.domain.common.vo.Contents;
+import touch.baton.domain.common.vo.Title;
+import touch.baton.domain.runnerpost.RunnerPost;
 import touch.baton.domain.runnerpost.repository.RunnerPostRepository;
+import touch.baton.domain.runnerpost.service.dto.RunnerPostCreateRequest;
+import touch.baton.domain.runnerpost.vo.Deadline;
+import touch.baton.domain.runnerpost.vo.PullRequestUrl;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -12,4 +18,23 @@ public class RunnerPostService {
 
     private final RunnerPostRepository runnerPostRepository;
 
+    public void update(final Long runnerPostId, final RunnerPostCreateRequest request) {
+        // TODO: 메소드 분리
+        RunnerPost presentRunnerPost = runnerPostRepository.findById(runnerPostId).get();
+
+        // TODO: 현재 로그인한 Runner 인지 확인
+        RunnerPost updatedRunnerPost = RunnerPost.builder()
+                .title(new Title(request.getTitle()))
+                .contents(new Contents(request.getContents()))
+                .pullRequestUrl(new PullRequestUrl(request.getPullRequestUrl()))
+                .deadline(Deadline.from(request.getDeadline()))
+                .watchedCount(presentRunnerPost.getWatchedCount())
+                .chattingRoomCount(presentRunnerPost.getChattingRoomCount())
+                .runnerPostTags(presentRunnerPost.getRunnerPostTags())
+                .runner(presentRunnerPost.getRunner())
+                .supporter(presentRunnerPost.getSupporter())
+                .build();
+
+        runnerPostRepository.save(updatedRunnerPost);
+    }
 }
--- backend/baton/src/main/java/touch/baton/domain/runnerpost/vo/Deadline.java
+++ backend/baton/src/main/java/touch/baton/domain/runnerpost/vo/Deadline.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Objects;
 
 import static lombok.AccessLevel.PROTECTED;
@@ -16,13 +18,24 @@ import static lombok.AccessLevel.PROTECTED;
 @Embeddable
 public class Deadline {
 
+    private static final String DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm";
+
     @Column(name = "deadline", nullable = false)
-    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    @DateTimeFormat(pattern = DATE_TIME_FORMAT)
     private LocalDateTime value;
 
     public Deadline(final LocalDateTime value) {
         validateNotNull(value);
         this.value = value;
+    }
+
+    public static Deadline from(final String input) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_TIME_FORMAT);
+        try {
+            return new Deadline(LocalDateTime.parse(input, formatter));
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("deadline 의 형식이 잘못되었습니다.");
+        }
     }
 
     private void validateNotNull(final LocalDateTime value) {
