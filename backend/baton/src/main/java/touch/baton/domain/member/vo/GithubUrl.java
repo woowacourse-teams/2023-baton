35cd174d5ca9a190c8609071b9f2c8619bc968bc
jeonghoon <shb03323@naver.com>
2023-07-18 13:10:12 +0900

    feat: GithubUrl null 예외 검증 구현

--- backend/baton/src/main/java/touch/baton/domain/member/vo/GithubUrl.java
+++ backend/baton/src/main/java/touch/baton/domain/member/vo/GithubUrl.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Embeddable;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 import static lombok.AccessLevel.PROTECTED;
 
 @Getter
@@ -12,10 +14,17 @@ import static lombok.AccessLevel.PROTECTED;
 @Embeddable
 public class GithubUrl {
 
-    @Column(name = "github_url")
+    @Column(name = "github_url", nullable = false)
     private String value;
 
     public GithubUrl(final String value) {
+        validateNotNull(value);
         this.value = value;
+    }
+
+    private void validateNotNull(final String value) {
+        if (Objects.isNull(value)) {
+            throw new IllegalArgumentException("githubUrl 은 null 일 수 없습니다.");
+        }
     }
 }
