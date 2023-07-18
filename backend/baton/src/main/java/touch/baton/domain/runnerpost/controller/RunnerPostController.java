9ba9315ea795d2ff19219d59b057625784612c5c
jeonghoon <shb03323@naver.com>
2023-07-18 23:42:05 +0900

    feat: Controller 레이어 구현

--- backend/baton/src/main/java/touch/baton/domain/runnerpost/controller/RunnerPostController.java
+++ backend/baton/src/main/java/touch/baton/domain/runnerpost/controller/RunnerPostController.java
@@ -1,9 +1,16 @@
 package touch.baton.domain.runnerpost.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import touch.baton.domain.runnerpost.service.RunnerPostService;
+import touch.baton.domain.runnerpost.service.dto.RunnerPostCreateRequest;
+
+import java.net.URI;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/posts/runner")
@@ -11,4 +18,12 @@ import touch.baton.domain.runnerpost.service.RunnerPostService;
 public class RunnerPostController {
 
     private final RunnerPostService runnerPostService;
+
+    @PatchMapping("/{runnerPostId}")
+    public ResponseEntity<Void> update(@PathVariable(name = "runnerPostId") final Long id,
+                                       @RequestBody final RunnerPostCreateRequest request
+    ) {
+        runnerPostService.update(id, request);
+        return ResponseEntity.created(URI.create("/api/v1/posts/runner/" + id)).build();
+    }
 }
