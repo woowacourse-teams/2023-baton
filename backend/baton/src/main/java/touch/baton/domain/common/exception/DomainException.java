e9bb454f87849fca7276678235a2ff874a3a2b4f
jeonghoon <shb03323@naver.com>
2023-07-18 13:11:15 +0900

    feat: Member null 예외 검증 구현

---
+++ backend/baton/src/main/java/touch/baton/domain/common/exception/DomainException.java
@@ -0,0 +1,8 @@
+package touch.baton.domain.common.exception;
+
+public class DomainException extends RuntimeException {
+
+    public DomainException(final String message) {
+        super(message);
+    }
+}
--- backend/baton/src/main/java/touch/baton/domain/member/Member.java
+++ backend/baton/src/main/java/touch/baton/domain/member/Member.java
@@ -7,13 +7,16 @@ import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import touch.baton.domain.member.exception.MemberException;
 import touch.baton.domain.member.vo.Company;
 import touch.baton.domain.member.vo.Email;
 import touch.baton.domain.member.vo.GithubUrl;
-import touch.baton.domain.member.vo.MemberId;
 import touch.baton.domain.member.vo.Name;
 import touch.baton.domain.member.vo.OauthId;
 
+import java.util.Objects;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
 @Getter
@@ -41,7 +44,7 @@ public class Member {
     private Company company;
 
     @Builder
-    public Member(final Name name,
+    private Member(final Name name,
                   final Email email,
                   final OauthId oauthId,
                   final GithubUrl githubUrl,
@@ -50,18 +53,46 @@ public class Member {
         this(null, name, email, oauthId, githubUrl, company);
     }
 
-    public Member(final MemberId id,
-                  final Name name,
-                  final Email email,
-                  final OauthId oauthId,
-                  final GithubUrl githubUrl,
-                  final Company company
+    private Member(final Long id,
+                   final Name name,
+                   final Email email,
+                   final OauthId oauthId,
+                   final GithubUrl githubUrl,
+                   final Company company
     ) {
+        validateNotNull(name, email, oauthId, githubUrl, company);
         this.id = id;
         this.name = name;
         this.email = email;
         this.oauthId = oauthId;
         this.githubUrl = githubUrl;
         this.company = company;
+    }
+
+    private void validateNotNull(final Name name,
+                                 final Email email,
+                                 final OauthId oauthId,
+                                 final GithubUrl githubUrl,
+                                 final Company company
+    ) {
+        if (Objects.isNull(name)) {
+            throw new MemberException.NotNull("name 는 null 일 수 없습니다.");
+        }
+
+        if (Objects.isNull(email)) {
+            throw new MemberException.NotNull("name 는 null 일 수 없습니다.");
+        }
+
+        if (Objects.isNull(oauthId)) {
+            throw new MemberException.NotNull("name 는 null 일 수 없습니다.");
+        }
+
+        if (Objects.isNull(githubUrl)) {
+            throw new MemberException.NotNull("name 는 null 일 수 없습니다.");
+        }
+
+        if (Objects.isNull(company)) {
+            throw new MemberException.NotNull("name 는 null 일 수 없습니다.");
+        }
     }
 }
