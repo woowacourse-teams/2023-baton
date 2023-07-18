c70f1cb737ba9f20cde9c79803f28c9ead34ea6a
guridaek <xylitol79@gmail.com>
2023-07-18 21:58:54 +0900

    feat: Tag 컴포넌트 구현

---
+++ FE/src/components/common/Tag.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+import closeIcon from '@assets/close-icon-red.svg';
+
+interface Props extends React.HTMLProps<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+const Tag = ({ children, onClick }: Props) => {
+  return (
+    <S.TagWrapper>
+      <S.Tag onClick={onClick}>
+        {`#${children}`}
+        <S.CloseTagIcon src={closeIcon} />
+      </S.Tag>
+    </S.TagWrapper>
+  );
+};
+
+export default Tag;
+
+const S = {
+  TagWrapper: styled.div`
+    :hover {
+      transition: all 0.35s ease;
+      transform: scale(1.04);
+    }
+  `,
+
+  Tag: styled.button`
+    display: flex;
+    align-items: center;
+
+    height: 34px;
+    padding: 0 15px;
+    gap: 5px;
+
+    background-color: white;
+    border-radius: 16px;
+    border: 1px solid var(--baton-red);
+
+    color: var(--baton-red);
+  `,
+
+  CloseTagIcon: styled.img`
+    width: 9px;
+    height: 9px;
+  `,
+};
