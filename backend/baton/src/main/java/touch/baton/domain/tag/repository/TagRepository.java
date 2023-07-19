067082414f9f0ed73eb0075d470486bb0245ae98
jeonghoon <shb03323@naver.com>
2023-07-20 01:56:35 +0900

    feat: tag 이름으로 tag 조회 기능 구현

--- backend/baton/src/main/java/touch/baton/domain/tag/repository/TagRepository.java
+++ backend/baton/src/main/java/touch/baton/domain/tag/repository/TagRepository.java
@@ -2,6 +2,12 @@ package touch.baton.domain.tag.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import touch.baton.domain.tag.Tag;
+import touch.baton.domain.tag.vo.TagName;
+
+import java.util.Optional;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
+
+    Optional<Tag> findByTagName(TagName tagName);
+
 }
---
+++ backend/baton/src/test/java/touch/baton/domain/tag/repository/TagRepositoryTest.java
@@ -0,0 +1,38 @@
+package touch.baton.domain.tag.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import touch.baton.config.JpaConfig;
+import touch.baton.domain.tag.Tag;
+import touch.baton.domain.tag.vo.TagName;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Import(JpaConfig.class)
+@DataJpaTest
+class TagRepositoryTest {
+
+    @Autowired
+    private TagRepository tagRepository;
+
+    @DisplayName("이름으로 Tag 를 조회할 때 Tag 가 있으면 조회된다.")
+    @Test
+    void findByTagName_exist() {
+        // given
+        String tagName = "java";
+        Tag expected = Tag.newInstance(tagName);
+        tagRepository.saveAndFlush(expected);
+
+        // when
+        Optional<Tag> actual = tagRepository.findByTagName(new TagName(tagName));
+
+        // then
+        assertThat(actual).isPresent();
+        assertThat(expected).isEqualTo(actual.get());
+    }
+}
