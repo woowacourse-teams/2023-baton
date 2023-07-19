eb73e0d53b4bd63e5afbcfc5a4030d93874f6f79
jeonghoon <shb03323@naver.com>
2023-07-20 01:58:04 +0900

    feat: 러너 게시글 수정 service 구현

--- backend/baton/src/main/java/touch/baton/domain/runnerpost/service/RunnerPostService.java
+++ backend/baton/src/main/java/touch/baton/domain/runnerpost/service/RunnerPostService.java
@@ -10,6 +10,15 @@ import touch.baton.domain.runnerpost.repository.RunnerPostRepository;
 import touch.baton.domain.runnerpost.service.dto.RunnerPostCreateRequest;
 import touch.baton.domain.runnerpost.vo.Deadline;
 import touch.baton.domain.runnerpost.vo.PullRequestUrl;
+import touch.baton.domain.tag.RunnerPostTag;
+import touch.baton.domain.tag.Tag;
+import touch.baton.domain.tag.repository.RunnerPostTagRepository;
+import touch.baton.domain.tag.repository.TagRepository;
+import touch.baton.domain.tag.vo.TagName;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -17,25 +26,59 @@ import touch.baton.domain.runnerpost.vo.PullRequestUrl;
 public class RunnerPostService {
 
     private final RunnerPostRepository runnerPostRepository;
+    private final RunnerPostTagRepository runnerPostTagRepository;
+    private final TagRepository tagRepository;
 
     @Transactional
     public Long update(final Long runnerPostId, final RunnerPostCreateRequest request) {
         // TODO: 메소드 분리
-        RunnerPost presentRunnerPost = runnerPostRepository.findById(runnerPostId).get();
+        RunnerPost runnerPost = runnerPostRepository.findById(runnerPostId).get();
+        runnerPost.updateTitle(new Title(request.getTitle()));
+        runnerPost.updateContents(new Contents(request.getContents()));
+        runnerPost.updatePullRequestUrl(new PullRequestUrl(request.getPullRequestUrl()));
+        runnerPost.updateDeadLine(new Deadline(request.getFormattedDateTime()));
 
-        // TODO: 현재 로그인한 Runner 인지 확인
-        RunnerPost updatedRunnerPost = RunnerPost.builder()
-                .title(new Title(request.getTitle()))
-                .contents(new Contents(request.getContents()))
-                .pullRequestUrl(new PullRequestUrl(request.getPullRequestUrl()))
-                .deadline(Deadline.from(request.getDeadline()))
-                .watchedCount(presentRunnerPost.getWatchedCount())
-                .chattingRoomCount(presentRunnerPost.getChattingRoomCount())
-                .runnerPostTags(presentRunnerPost.getRunnerPostTags())
-                .runner(presentRunnerPost.getRunner())
-                .supporter(presentRunnerPost.getSupporter())
-                .build();
+        List<RunnerPostTag> presentRunnerPostTags = runnerPostTagRepository.joinTagsByRunnerPostId(runnerPost.getId());
+        // TODO: tag 개수 차감 메소드 분리
+        List<Tag> presentTags = presentRunnerPostTags.stream()
+                .map(RunnerPostTag::getTag)
+                .toList();
+        presentTags.forEach(Tag::decreaseCount);
 
-        return runnerPostRepository.save(updatedRunnerPost).getId();
+        // TODO: 새로운 tag 로 교체 메소드 분리
+        List<RunnerPostTag> removedRunnerPostTags = new ArrayList<>(presentRunnerPostTags);
+        for (String tagName : request.getTags()) {
+            Optional<RunnerPostTag> existRunnerPostTag = presentRunnerPostTags.stream()
+                    .filter(presentRunnerPostTag -> presentRunnerPostTag.isSameTagName(tagName))
+                    .findFirst();
+            if (existRunnerPostTag.isPresent()) {
+                removedRunnerPostTags.remove(existRunnerPostTag.get());
+                existRunnerPostTag.get().getTag().increaseCount();
+            }
+            existRunnerPostTag.ifPresent(removedRunnerPostTags::remove);
+            if (existRunnerPostTag.isEmpty()) {
+                // TODO: tag 찾기 메소드 분리
+                Optional<Tag> tag = tagRepository.findByTagName(new TagName(tagName));
+                if (tag.isEmpty()) {
+                    Tag newTag = tagRepository.save(Tag.newInstance(tagName));
+                    RunnerPostTag newRunnerPostTag = runnerPostTagRepository.save(RunnerPostTag.builder()
+                            .runnerPost(runnerPost)
+                            .tag(newTag)
+                            .build());
+                    runnerPost.appendRunnerPostTag(newRunnerPostTag);
+                }
+                if (tag.isPresent()) {
+                    tag.get().increaseCount();
+                    RunnerPostTag newRunnerPostTag = runnerPostTagRepository.save(RunnerPostTag.builder()
+                            .runnerPost(runnerPost)
+                            .tag(tag.get())
+                            .build());
+                    runnerPost.appendRunnerPostTag(newRunnerPostTag);
+                }
+            }
+        }
+        runnerPostTagRepository.deleteAll(removedRunnerPostTags);
+
+        return runnerPost.getId();
     }
 }
