1dfc8e97b50fd737175a0a9ab1dd79b650a4f817
eunbii0213 <eunbii0213@naver.com>
2023-07-19 19:04:20 +0900

    feat: 리뷰 요청 글 서비스 구현

--- backend/baton/src/main/java/touch/baton/domain/runnerpost/service/RunnerPostService.java
+++ backend/baton/src/main/java/touch/baton/domain/runnerpost/service/RunnerPostService.java
@@ -3,7 +3,11 @@ package touch.baton.domain.runnerpost.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import touch.baton.domain.common.vo.Title;
+import touch.baton.domain.runnerpost.RunnerPost;
 import touch.baton.domain.runnerpost.repository.RunnerPostRepository;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -12,4 +16,23 @@ public class RunnerPostService {
 
     private final RunnerPostRepository runnerPostRepository;
 
+    //response가 json형식인지 test
+    //mockMvcTest 이용해서 ObjectMapper. json -> string으로 변환해서 맞는지 테스트
+    public List<RunnerPost> read() {
+        return runnerPostRepository.findAll();
+    }
+
+    public List<RunnerPost> findByRunnerId(Long runnerId) {
+        return runnerPostRepository.findByRunnerId(runnerId);
+    }
+
+    public List<RunnerPost> findBySupporterId(Long supporterId) {
+        return runnerPostRepository.findBySupporterId(supporterId);
+    }
+
+    //문맥이 조금 어색하지만 메세지 가장 앞에 title임을 보여주기 위함
+    public RunnerPost findByTitle(String title) {
+        return runnerPostRepository.findByTitle(new Title(title))
+                .orElseThrow(() -> new IllegalArgumentException("title이 제목인 post가 존재하지 않습니다."));
+    }
 }
