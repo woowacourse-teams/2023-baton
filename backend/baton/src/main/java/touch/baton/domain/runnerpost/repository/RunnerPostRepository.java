75dbe7e860923e3e0601a7c52d0bcbbc275cb96f
goatsfish <goatsfish@gmail.com>
2023-07-19 15:58:21 +0900

    feat: RunnerPost 상세 조회 및 삭제 기능 추가

--- backend/baton/src/main/java/touch/baton/domain/runnerpost/repository/RunnerPostRepository.java
+++ backend/baton/src/main/java/touch/baton/domain/runnerpost/repository/RunnerPostRepository.java
@@ -1,7 +1,19 @@
 package touch.baton.domain.runnerpost.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import touch.baton.domain.runnerpost.RunnerPost;
 
+import java.util.Optional;
+
 public interface RunnerPostRepository extends JpaRepository<RunnerPost, Long> {
+
+    @Query(value = """
+            select rp
+            from RunnerPost rp
+            join fetch Runner r on r.id = rp.runner.id
+            join fetch Member m on m.id = r.member.id
+            where rp.id = :runnerPostId
+            """)
+    Optional<RunnerPost> joinMemberByRunnerPostId(final Long runnerPostId);
 }
