a1c7808ea26620d90665342c807de16853ce169e
eunbii0213 <eunbii0213@naver.com>
2023-07-19 19:04:03 +0900

    feat: 리뷰 요청 글 레포지토리 구현

--- backend/baton/src/main/java/touch/baton/domain/runnerpost/repository/RunnerPostRepository.java
+++ backend/baton/src/main/java/touch/baton/domain/runnerpost/repository/RunnerPostRepository.java
@@ -1,7 +1,16 @@
 package touch.baton.domain.runnerpost.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import touch.baton.domain.common.vo.Title;
 import touch.baton.domain.runnerpost.RunnerPost;
+import touch.baton.domain.tag.RunnerPostTags;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface RunnerPostRepository extends JpaRepository<RunnerPost, Long> {
+
+    List<RunnerPost> findByRunnerId(Long runnerId);
+    List<RunnerPost> findBySupporterId(Long supporterId);
+    Optional<RunnerPost> findByTitle(Title title);
 }
