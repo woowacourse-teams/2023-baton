fa49284b27c6f21fae871b09a5027d12b15a2a95
goatsfish <goatsfish@gmail.com>
2023-07-19 16:07:09 +0900

    feat: Tag 레포지터리에 TagCount 감소 기능 구현

--- backend/baton/src/main/java/touch/baton/domain/tag/repository/TagRepository.java
+++ backend/baton/src/main/java/touch/baton/domain/tag/repository/TagRepository.java
@@ -1,7 +1,20 @@
 package touch.baton.domain.tag.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import touch.baton.domain.tag.Tag;
 
+import java.util.List;
+
 public interface TagRepository extends JpaRepository<Tag, Long> {
+
+    @Modifying(clearAutomatically = true)
+    @Query("""
+        update Tag t
+        set t.tagCount.value = t.tagCount.value - 1
+        where t.id in :tagIds
+        """)
+    void decreaseTagCountByTagIds(@Param("tagIds") final List<Long> tagIds);
 }
---
+++ backend/baton/src/test/java/touch/baton/domain/tag/repository/TagRepositoryUpdateTest.java
@@ -0,0 +1,50 @@
+package touch.baton.domain.tag.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import touch.baton.config.RepositoryTestConfig;
+import touch.baton.domain.tag.Tag;
+import touch.baton.domain.tag.vo.TagCount;
+import touch.baton.domain.tag.vo.TagName;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class TagRepositoryUpdateTest extends RepositoryTestConfig {
+
+    @Autowired
+    private TagRepository tagRepository;
+
+    @DisplayName("태그 식별자값 목록으로 TagCount 수를 -1 씩 줄인다.")
+    @Test
+    void decreaseTagCountByTagIds() {
+        // given
+        final Tag tag = Tag.builder()
+                .tagName(new TagName("자바"))
+                .tagCount(new TagCount(100))
+                .build();
+
+        final Tag save = tagRepository.save(tag);
+
+        // when
+        tagRepository.decreaseTagCountByTagIds(List.of(tag.getId()));
+        final Optional<Tag> maybeTag = tagRepository.findById(tag.getId());
+
+        // then
+        assertAll(
+                () -> assertThat(maybeTag).isPresent(),
+                () -> assertThat(maybeTag.get())
+                        .usingRecursiveComparison()
+                        .ignoringExpectedNullFields()
+                        .isEqualTo(Tag.builder()
+                                .tagName(new TagName("자바"))
+                                .tagCount(new TagCount(99))
+                                .build())
+        );
+    }
+
+}
