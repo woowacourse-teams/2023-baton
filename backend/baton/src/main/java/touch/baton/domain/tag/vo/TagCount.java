cc04fab2890a3e96fc55f27599c320768847a5e1
Ethan <kim76744dev@gmail.com>
2023-07-19 14:41:25 +0900

    feat: Tag 기본 생성 기능 및 count 증가 기능 추가

--- backend/baton/src/main/java/touch/baton/domain/tag/vo/TagCount.java
+++ backend/baton/src/main/java/touch/baton/domain/tag/vo/TagCount.java
@@ -2,18 +2,21 @@ package touch.baton.domain.tag.vo;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 
 import static lombok.AccessLevel.PROTECTED;
 
+@EqualsAndHashCode
 @Getter
 @NoArgsConstructor(access = PROTECTED)
 @Embeddable
 public class TagCount {
 
     private static final String DEFAULT_VALUE = "1";
+    private static final TagCount INITIAL_TAG_COUNT = new TagCount(Integer.parseInt(DEFAULT_VALUE));
 
     @ColumnDefault(DEFAULT_VALUE)
     @Column(name = "tag_count", nullable = false)
@@ -21,5 +24,13 @@ public class TagCount {
 
     public TagCount(final int value) {
         this.value = value;
+    }
+
+    public static TagCount zero() {
+        return INITIAL_TAG_COUNT;
+    }
+
+    public TagCount increase() {
+        return new TagCount(value + 1);
     }
 }
