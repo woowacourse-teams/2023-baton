d48c94cf572085e56dec98910ed67bd7a1c74f24
gyeongza <gyeongza@naver.com>
2023-07-19 16:47:19 +0900

    fix: msw dev에서만 작동하도록 수정

--- FE/src/index.tsx
+++ FE/src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 import { GlobalStyle } from './styles/GlobalStyles';
+import { worker } from './mocks/worker';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
 
 root.render(
   <React.StrictMode>
