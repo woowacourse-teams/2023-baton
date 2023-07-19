108e36733e40fa2a7128d0c7e9cbeaa0ba9a9fc0
Ethan <kim76744dev@gmail.com>
2023-07-19 15:16:59 +0900

    feat: WatchedCount 기본 생성 기능 구현

--- backend/baton/src/main/java/touch/baton/domain/common/vo/WatchedCount.java
+++ backend/baton/src/main/java/touch/baton/domain/common/vo/WatchedCount.java
@@ -16,6 +16,7 @@ import static lombok.AccessLevel.PROTECTED;
 public class WatchedCount {
 
     private static final String DEFAULT_VALUE = "0";
+    private static final WatchedCount ZERO = new WatchedCount(Integer.parseInt(DEFAULT_VALUE));
 
     @ColumnDefault(DEFAULT_VALUE)
     @Column(name = "watch_count", nullable = false)
@@ -23,5 +24,9 @@ public class WatchedCount {
 
     public WatchedCount(final int value) {
         this.value = value;
+    }
+
+    public static WatchedCount zero() {
+        return ZERO;
     }
 }
