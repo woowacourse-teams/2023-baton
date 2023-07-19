01457b575985c70e8ae1a824cc43b29ae69fdcc2
Ethan <kim76744dev@gmail.com>
2023-07-19 17:03:13 +0900

    feat: RunnerPost 생성 API 구현

--- backend/baton/src/main/java/touch/baton/domain/runnerpost/service/RunnerPostService.java
+++ backend/baton/src/main/java/touch/baton/domain/runnerpost/service/RunnerPostService.java
@@ -3,7 +3,16 @@ package touch.baton.domain.runnerpost.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import touch.baton.domain.runner.Runner;
+import touch.baton.domain.runnerpost.RunnerPost;
 import touch.baton.domain.runnerpost.repository.RunnerPostRepository;
+import touch.baton.domain.runnerpost.service.dto.RunnerPostCreateRequest;
+import touch.baton.domain.tag.RunnerPostTag;
+import touch.baton.domain.tag.Tag;
+import touch.baton.domain.tag.repository.TagRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -11,5 +20,49 @@ import touch.baton.domain.runnerpost.repository.RunnerPostRepository;
 public class RunnerPostService {
 
     private final RunnerPostRepository runnerPostRepository;
+    private final TagRepository tagRepository;
 
+    @Transactional
+    public Long create(final Runner runner, final RunnerPostCreateRequest request) {
+        RunnerPost runnerPost = toDomain(runner, request);
+
+        final List<Tag> tags = request.tags().stream()
+                .map(Tag::newInstance)
+                .toList();
+
+        for (final Tag tag : tags) {
+            final Optional<Tag> findTag = tagRepository.findByTagName(tag.getTagName());
+
+            if (findTag.isEmpty()) {
+                tagRepository.save(tag);
+                continue;
+            }
+
+            final Tag presentTag = findTag.get();
+            presentTag.increaseCount();
+        }
+
+        runnerPostRepository.save(runnerPost);
+
+        final List<RunnerPostTag> postTags = tags.stream()
+                .map(tag -> RunnerPostTag.builder()
+                        .tag(tag)
+                        .runnerPost(runnerPost)
+                        .build())
+                .toList();
+
+        for (final RunnerPostTag postTag : postTags) {
+            runnerPost.addPostTag(postTag);
+        }
+
+        return runnerPost.getId();
+    }
+
+    private RunnerPost toDomain(final Runner runner, final RunnerPostCreateRequest request) {
+        return RunnerPost.newInstance(request.title(),
+                request.contents(),
+                request.pullRequestUrl(),
+                request.deadline(),
+                runner);
+    }
 }
---
+++ backend/baton/src/test/java/touch/baton/domain/tag/RunnerPostTagsTest.java
@@ -0,0 +1,23 @@
+package touch.baton.domain.tag;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RunnerPostTagsTest {
+
+    @DisplayName("RunnerPostTags 에 runnerPostTag 를 추가할 수 있다.")
+    @Test
+    void addRunnerPostTags() {
+        // given
+//        RunnerPostTags postTags = new RunnerPostTags();
+//        RunnerPostTag.builder()
+//                .runnerPost()
+//                .tag(Tag.newInstance("Java"))
+//                .build()
+//
+//        // when
+//        postTags.add();
+        
+        // then
+    }
+}
