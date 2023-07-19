f70f4d9ee984d26d251ddd5defd1939b05baeaee
에이든 <62369936+gyeongza@users.noreply.github.com>
2023-07-19 17:00:55 +0900

    msw, tsconfig, Avatar 공통 변경사항 반영 (#27)
    * chore: json 파일을 import하기 위한 tsconfig 수정
    
    * fix: msw dev에서만 작동하도록 수정
    
    * design: Avatar 기본 사이즈 설정

--- FE/src/components/common/Avatar.tsx
+++ FE/src/components/common/Avatar.tsx
@@ -19,8 +19,8 @@ const S = {
   ImageWrapper: styled.div``,
 
   Image: styled.img<{ $width?: string | number; $height?: string | number }>`
-    width: ${({ $width }) => $width || '100%'};
-    height: ${({ $height }) => $height || '100%'};
+    width: ${({ $width }) => $width || '60px'};
+    height: ${({ $height }) => $height || '60px'};
 
     border: 0.5px solid var(--gray-800);
     border-radius: 50%;
