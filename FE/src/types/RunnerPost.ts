b6bf12722efba774fc4fffbbac1ce176a3f82e76
gyeongza <gyeongza@naver.com>
2023-07-20 20:19:23 +0900

    fix: api 명세 변경에 따른 러너 글 타입 수정

--- FE/src/types/RunnerPost.ts
+++ FE/src/types/RunnerPost.ts
@@ -1,7 +1,13 @@
-export interface RunnerPost {
+export interface RunnerPostData {
   runnerPostId: number;
   title: string;
   deadline: string;
   tags: string[];
   profile: { name: string; imageUrl: string };
+  watchedCount: number;
+  chattingCount: number;
+}
+
+export interface RunnerPost {
+  data: RunnerPostData[];
 }
