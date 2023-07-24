207d9fd67d7cddb232da684ba032ed0e5b5fac92
gyeongza <gyeongza@naver.com>
2023-07-24 16:51:08 +0900

    fix: dev server 새로고침 오류 수정

--- FE/webpack/webpack.dev.js
+++ FE/webpack/webpack.dev.js
@@ -4,6 +4,9 @@ const common = require('./webpack.common');
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
+  output: {
+    publicPath: '/',
+  },
   devServer: {
     open: true,
     hot: true,
