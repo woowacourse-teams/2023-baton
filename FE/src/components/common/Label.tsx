de16a5fa78796ce99aad92654c7561b2c7aa6754
KangSan Lee <xylitol79@gmail.com>
2023-07-25 17:28:58 +0900

    Label 컴포넌트 구현 (#88)
    feat: Label 컴포넌트 구현

---
+++ FE/src/components/common/Label.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+interface Props extends React.HTMLProps<HTMLDivElement> {
+  children: React.ReactNode;
+  colorTheme: 'RED' | 'GRAY';
+  fontSize?: string | number;
+  fontWeight?: number;
+}
+
+const Label = ({ children, width, height, colorTheme, fontSize, fontWeight }: Props) => {
+  return (
+    <S.LabelWrapper>
+      <S.Label $width={width} $height={height} $colorTheme={colorTheme} $fontSize={fontSize} $fontWeight={fontWeight}>
+        {children}
+      </S.Label>
+    </S.LabelWrapper>
+  );
+};
+
+export default Label;
+
+const S = {
+  LabelWrapper: styled.div``,
+
+  Label: styled.div<{
+    $colorTheme: 'RED' | 'GRAY';
+    $width?: string | number;
+    $height?: string | number;
+    $fontSize?: string | number;
+    $fontWeight?: number;
+  }>`
+    ${({ $colorTheme }) => themeStyles[$colorTheme]}
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: ${({ $width }) => $width || 'fit-content'};
+    height: ${({ $height }) => $height || '22px'};
+    border-radius: 16px;
+    padding: 10px 10px;
+
+    font-size: ${({ $fontSize }) => $fontSize || '12px'};
+    font-weight: ${({ $fontWeight }) => $fontWeight || '400'};
+  `,
+};
+
+const themeStyles = {
+  RED: css`
+    border: 1px solid var(--baton-red);
+
+    color: var(--baton-red);
+  `,
+  GRAY: css`
+    background: var(--gray-500);
+
+    color: white;
+  `,
+};
