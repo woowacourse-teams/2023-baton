e6b92ddd77203f4ede27be962d0a3cb8ce904c82
guridaek <xylitol79@gmail.com>
2023-07-25 17:01:36 +0900

    feat: reviewStatus 타입 및 Label text 추가

--- FE/src/constants/index.ts
+++ FE/src/constants/index.ts
@@ -1,1 +1,9 @@
+import { ReviewStatus } from '@/types/runnerPost';
+
 export const BATON_BASE_URL = 'http://15.164.179.87:80/api/v1';
+
+export const REVIEW_STATUS_LABEL_TEXT: { [key in ReviewStatus]: string } = {
+  NOT_STARTED: '리뷰 대기중',
+  IN_PROGRESS: '리뷰 진행중',
+  DONE: '리뷰 완료',
+};
