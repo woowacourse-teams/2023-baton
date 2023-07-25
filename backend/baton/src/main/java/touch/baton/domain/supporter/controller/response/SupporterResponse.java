3c92b0b3dd55a13ce6bcf26e9e82dc1dad38a6a1
ditoo <shb03323@naver.com>
2023-07-25 17:04:43 +0900

    feat: Supporter Response 구현

---
+++ backend/baton/src/main/java/touch/baton/domain/supporter/controller/response/SupporterResponse.java
@@ -0,0 +1,26 @@
+package touch.baton.domain.supporter.controller.response;
+
+import touch.baton.domain.supporter.Supporter;
+
+public record SupporterResponse() {
+
+    public record Detail(Long supporterId,
+                         String name,
+                         int reviewCount,
+                         int totalRating,
+                         String githubUrl,
+                         String introduction
+    ) {
+
+        public static Detail from(final Supporter supporter) {
+            return new Detail(
+                    supporter.getId(),
+                    supporter.getMember().getMemberName().getValue(),
+                    supporter.getReviewCount().getValue(),
+                    supporter.getTotalRating().getValue(),
+                    supporter.getMember().getGithubUrl().getValue(),
+                    supporter.getIntroduction().getValue()
+            );
+        }
+    }
+}
