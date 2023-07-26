180eb8cd0ac1b617d28f25249cc686544a687d21
ditoo <shb03323@naver.com>
2023-07-26 18:07:23 +0900

    feat: ValidFuture 어노테이션 추가

---
+++ backend/baton/src/main/java/touch/baton/domain/runnerpost/exception/validator/ValidFuture.java
@@ -0,0 +1,27 @@
+package touch.baton.domain.runnerpost.exception.validator;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import touch.baton.domain.common.exception.ClientErrorCode;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+@Retention(RUNTIME)
+@Documented
+@Constraint(validatedBy = FutureValidator.class)
+public @interface ValidFuture {
+
+    String message() default "마감일은 오늘보다 과거일 수 없습니다.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    ClientErrorCode clientErrorCode();
+}
