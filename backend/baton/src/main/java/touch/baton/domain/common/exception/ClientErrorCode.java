def3a3bdcb396f9a091ed68cfb0b04d45a63894e
ditoo <shb03323@naver.com>
2023-07-26 17:24:08 +0900

    refactor: ClientErrorCode 내용 추가

--- backend/baton/src/main/java/touch/baton/domain/common/exception/ClientErrorCode.java
+++ backend/baton/src/main/java/touch/baton/domain/common/exception/ClientErrorCode.java
@@ -3,7 +3,16 @@ package touch.baton.domain.common.exception;
 import org.springframework.http.HttpStatus;
 
 public enum ClientErrorCode implements ErrorCode {
-    ;
+
+    TITLE_IS_NULL(HttpStatus.BAD_REQUEST, "RP001", "제목을 입력해주세요."),
+    PULL_REQUEST_URL_IS_NULL(HttpStatus.BAD_REQUEST, "RP002", "PR 주소를 입력해주세요."),
+    DEADLINE_IS_NULL(HttpStatus.BAD_REQUEST, "RP003", "마감일을 입력해주세요."),
+    CONTENTS_ARE_NULL(HttpStatus.BAD_REQUEST, "RP004", "내용을 입력해주세요."),
+    CONTENTS_OVERFLOW(HttpStatus.BAD_REQUEST, "RP005", "내용은 1000자 까지 입력해주세요."),
+    PAST_DEADLINE(HttpStatus.BAD_REQUEST, "RP006", "마감일은 오늘보다 과거일 수 없습니다."),
+    CONTENTS_NOT_FOUND(HttpStatus.NOT_FOUND, "RP007", "존재하지 않는 게시물입니다."),
+    TAGS_ARE_NULL(HttpStatus.NOT_FOUND, "RP008", "태그 목록을 빈 값이라도 입력해주세요.");
+
     private final HttpStatus httpStatus;
     private final String errorCode;
     private final String message;
