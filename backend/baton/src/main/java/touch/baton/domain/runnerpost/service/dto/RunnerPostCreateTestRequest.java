4b29a173b349d205db2aeed0568e14004be35fff
Ethan <kim76744dev@gmail.com>
2023-07-26 17:01:11 +0900

    refactor: RunnerPostCreateTestRequest 에서 컨버터를 사용하도록 변경

--- backend/baton/src/main/java/touch/baton/domain/runnerpost/service/dto/RunnerPostCreateTestRequest.java
+++ backend/baton/src/main/java/touch/baton/domain/runnerpost/service/dto/RunnerPostCreateTestRequest.java
@@ -1,14 +1,11 @@
 package touch.baton.domain.runnerpost.service.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
 public record RunnerPostCreateTestRequest(String title,
                                           List<String> tags,
                                           String pullRequestUrl,
-                                          @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm", timezone = "Asia/Seoul")
                                           LocalDateTime deadline,
                                           String contents,
                                           Long supporterId
