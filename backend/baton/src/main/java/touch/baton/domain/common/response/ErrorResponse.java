13df7f09b71b849d6a8932cd14dd1af2043f6ce7
ditoo <shb03323@naver.com>
2023-07-26 17:08:26 +0900

    refactor: ErrorResponse 에서 httpStatus 제거

--- backend/baton/src/main/java/touch/baton/domain/common/response/ErrorResponse.java
+++ backend/baton/src/main/java/touch/baton/domain/common/response/ErrorResponse.java
@@ -1,21 +1,18 @@
 package touch.baton.domain.common.response;
 
-import org.springframework.http.HttpStatus;
 import touch.baton.domain.common.exception.BaseException;
 
 public class ErrorResponse {
 
-    private final HttpStatus status;
     private final String errorCode;
     private final String message;
 
-    private ErrorResponse(final HttpStatus status, final String errorCode, final String message) {
-        this.status = status;
+    private ErrorResponse(final String errorCode, final String message) {
         this.errorCode = errorCode;
         this.message = message;
     }
 
     public static ErrorResponse from(final BaseException e) {
-        return new ErrorResponse(e.getHttpStatus(), e.getErrorCode(), e.getMessage());
+        return new ErrorResponse(e.getErrorCode(), e.getMessage());
     }
 }
