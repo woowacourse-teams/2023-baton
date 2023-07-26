6ab6e7d2bef08d7c4ad7c3812f12fab6f3f1e4ff
ditoo <shb03323@naver.com>
2023-07-26 17:51:57 +0900

    feat: NotNullValid 어노테이션 추가

---
+++ backend/baton/src/main/java/touch/baton/domain/common/exception/validator/NotNullValid.java
@@ -0,0 +1,36 @@
+package touch.baton.domain.common.exception.validator;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import touch.baton.domain.common.exception.ClientErrorCode;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+@Retention(RUNTIME)
+@Repeatable(NotNullValid.List.class)
+@Documented
+@Constraint(validatedBy = NotNullValidator.class)
+public @interface NotNullValid {
+
+    String message() default "null 값이 존재합니다.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+    ClientErrorCode clientErrorCode();
+
+    @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+    @Retention(RUNTIME)
+    @Documented
+    @interface List {
+
+        NotNullValid[] value();
+    }
+}
