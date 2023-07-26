45b5ce2fec1a95e927b91a240d6d4f3db3cbd852
hyena <goatsfish@gmail.com>
2023-07-26 15:02:52 +0900

    test: Runner Test Fixture 수정

--- backend/baton/src/test/java/touch/baton/assure/runnerpost/RunnerPostAssuredReadTest.java
+++ backend/baton/src/test/java/touch/baton/assure/runnerpost/RunnerPostAssuredReadTest.java
@@ -14,6 +14,7 @@ import touch.baton.fixture.domain.RunnerPostFixture;
 import java.time.LocalDateTime;
 
 import static touch.baton.fixture.vo.DeadlineFixture.deadline;
+import static touch.baton.fixture.vo.TotalRatingFixture.totalRating;
 
 @SuppressWarnings("NonAsciiCharacters")
 class RunnerPostAssuredReadTest extends AssuredTestConfig {
@@ -29,7 +30,7 @@ class RunnerPostAssuredReadTest extends AssuredTestConfig {
         );
         memberRepository.save(member);
 
-        final Runner runner = RunnerFixture.from(member, 0, Grade.BARE_FOOT);
+        final Runner runner = RunnerFixture.from(member, totalRating(0), Grade.BARE_FOOT);
         runnerRepository.save(runner);
 
         final RunnerPost runnerPost = RunnerPostFixture.create(runner, deadline(LocalDateTime.now().plusHours(100)));
