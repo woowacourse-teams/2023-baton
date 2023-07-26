3b3ae5fde4ac7175781183d119c2971d5e6f9670
상규 <tkdrb@sang-gyuui-MacBookPro.local>
2023-07-26 10:44:38 +0900

    feat: SupporterSelectItem, List 컴포넌트 추가

---
+++ FE/src/components/SupporterSelect/SupporterSelectList/SupporterSelectList.tsx
@@ -0,0 +1,58 @@
+import { BATON_BASE_URL } from '@/constants';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import SupporterSelectItem from '../SupporterSelectItem/SupporterSelectItem';
+import { SupporterCardList } from '@/types/SupporterSelect';
+
+interface Props {
+  handleClickReviewButton: () => void;
+}
+
+const SupporterSelectList = ({ handleClickReviewButton }: Props) => {
+  const [supporterCardList, setSupporterCardList] = useState<SupporterCardList | null>(null);
+
+  const getSupporterCardList = async () => {
+    try {
+      const response = await fetch(`${BATON_BASE_URL}/supporters/test`, {
+        method: 'GET',
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+
+      const supporterCardList = await response.json();
+
+      return supporterCardList;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchRunnerPost = async () => {
+      const result = await getSupporterCardList();
+      setSupporterCardList(result);
+    };
+
+    fetchRunnerPost();
+  }, []);
+
+  return (
+    <S.SupporterSelectListContainer>
+      {supporterCardList?.data.map((card) => (
+        <SupporterSelectItem key={card.supporterId} handleClickReviewButton={handleClickReviewButton} {...card} />
+      ))}
+    </S.SupporterSelectListContainer>
+  );
+};
+
+export default SupporterSelectList;
+
+const S = {
+  SupporterSelectListContainer: styled.ul`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr 1fr);
+    column-gap: 30px;
+  `,
+};
