185a6676dd644b663606187137504356b18c88f0
Ethan <kim76744dev@gmail.com>
2023-07-27 17:01:35 +0900

    test: ConverterConfigTest를 SpringBootTest 에서 WebMvcTest 로 변경

--- backend/baton/src/test/java/touch/baton/config/converter/ConverterConfigTest.java
+++ backend/baton/src/test/java/touch/baton/config/converter/ConverterConfigTest.java
@@ -2,28 +2,20 @@ package touch.baton.config.converter;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@SpringBootTest(webEnvironment = RANDOM_PORT)
+@WebMvcTest(ConverterConfig.class)
 class ConverterConfigTest {
 
     @Autowired
     private ObjectMapper objectMapper;
-
-    @BeforeEach
-    void setup(@LocalServerPort int port) {
-
-    }
 
     @DisplayName("직렬화 할 때 LocalDateTime yyyy-MM-dd'T'HH:mm 형식으로 변경된다.")
     @Test
