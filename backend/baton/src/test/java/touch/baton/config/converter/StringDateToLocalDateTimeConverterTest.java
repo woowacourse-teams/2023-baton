a987811d45a55ffb14b4b0567ca6d111a119aa1b
Ethan <kim76744dev@gmail.com>
2023-07-27 17:34:43 +0900

    test: StringDateToLocalDateTimeConverter 실패 테스트 추가

--- backend/baton/src/test/java/touch/baton/config/converter/StringDateToLocalDateTimeConverterTest.java
+++ backend/baton/src/test/java/touch/baton/config/converter/StringDateToLocalDateTimeConverterTest.java
@@ -2,11 +2,15 @@ package touch.baton.config.converter;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
 import java.util.TimeZone;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StringDateToLocalDateTimeConverterTest {
 
@@ -15,7 +19,7 @@ class StringDateToLocalDateTimeConverterTest {
 
     @DisplayName("String 값 Date 가 LocalDateTime 으로 변경되는지 확인한다.")
     @Test
-    void convertStringDateToLocalDateTime() {
+    void success_convertStringDateToLocalDateTime() {
         // given
         final String expect = "2023-08-18T09:45";
         final StringDateToLocalDateTimeConverter converter = new StringDateToLocalDateTimeConverter(DEFAULT_DATE_TIME_FORMAT, KOREA_TIME_ZONE);
@@ -25,5 +29,16 @@ class StringDateToLocalDateTimeConverterTest {
 
         // then
         assertThat(actual).isEqualTo(LocalDateTime.of(2023, 8, 18, 9, 45));
+    }
+
+    @DisplayName("형식에 맞지 않는 String 값 Date 는 convert 할 때 예외를 던진다.")
+    @ValueSource(strings = {"2023-08-18T09:45:12", "2023-08-18 09:45", "2023/08/18 09:45", "2023.08.18 09:45"})
+    @ParameterizedTest
+    void fail_convertInvalidStringDateToLocalDateTime(final String expect) {
+        // given
+        final StringDateToLocalDateTimeConverter converter = new StringDateToLocalDateTimeConverter(DEFAULT_DATE_TIME_FORMAT, KOREA_TIME_ZONE);
+
+        // when, then
+        assertThatThrownBy(() -> converter.convert(expect)).isInstanceOf(DateTimeParseException.class);
     }
 }
