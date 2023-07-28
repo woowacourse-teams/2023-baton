055495f5aeef3f88983570e414697f27107170b7
gyeongza <gyeongza@naver.com>
2023-07-28 15:28:41 +0900

    feat: 변경된 api 명세에 따른 msw uri 수정

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -4,27 +4,27 @@ import runnerPostDetails from './data/runnerPostDetails.json';
 import supporterCardList from './data/supporterCardList.json';
 
 export const handlers = [
-  rest.post('*/posts/runner', async (req, res, ctx) => {
+  rest.post('*/posts/runner/test', async (req, res, ctx) => {
     const { title, tags, pullRequestUrl, deadline, contents } = await req.json();
 
     return res(ctx.delay(300), ctx.status(201), ctx.set('Content-Type', 'application/json'));
   }),
 
-  rest.get('*/posts/runner', async (_, res, ctx) => {
+  rest.get('*/posts/runner/test', async (_, res, ctx) => {
     return res(ctx.delay(300), ctx.status(200), ctx.json(runnerPostList));
   }),
 
-  rest.put('*/posts/runner/:runnerPostId', async (req, res, ctx) => {
+  rest.put('*/posts/runner/:runnerPostId/test', async (req, res, ctx) => {
     const { title, tags, pullRequestUrl, deadline, contents } = await req.json();
 
     return res(ctx.delay(300), ctx.status(201), ctx.set('Content-Type', 'application/json'));
   }),
 
-  rest.delete('*/posts/runner/:runnerPostId', async (req, res, ctx) => {
+  rest.delete('*/posts/runner/:runnerPostId/test', async (req, res, ctx) => {
     return res(ctx.delay(300), ctx.status(204), ctx.set('Content-Type', 'application/json'));
   }),
 
-  rest.get('*/posts/runner/:runnerPostId', async (req, res, ctx) => {
+  rest.get('*/posts/runner/:runnerPostId/test', async (req, res, ctx) => {
     return res(
       ctx.delay(300),
       ctx.status(200),
