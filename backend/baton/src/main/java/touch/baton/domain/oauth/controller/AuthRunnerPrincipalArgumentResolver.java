30b1474c9a33e5b12d5b17991f3e18806bf4b04b
hyena <goatsfish@gmail.com>
2023-08-02 07:39:45 +0900

    feat: Runner, Supporter Principal 어노테이션 및 Runner, Supporter ArgumentResolver 구현

---
+++ backend/baton/src/main/java/touch/baton/domain/oauth/controller/AuthRunnerPrincipalArgumentResolver.java
@@ -0,0 +1,45 @@
+package touch.baton.domain.oauth.controller;
+
+import com.auth0.jwt.interfaces.Claim;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import touch.baton.domain.oauth.repository.OauthRunnerRepository;
+import touch.baton.domain.runner.Runner;
+import touch.baton.infra.auth.jwt.JwtDecoder;
+
+import java.util.Map;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@RequiredArgsConstructor
+@Component
+public class AuthRunnerPrincipalArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final JwtDecoder jwtDecoder;
+    private final OauthRunnerRepository oauthRunnerRepository;
+
+    @Override
+    public boolean supportsParameter(final MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(AuthRunnerPrincipal.class);
+    }
+
+    @Override
+    public Object resolveArgument(final MethodParameter parameter,
+                                  final ModelAndViewContainer mavContainer,
+                                  final NativeWebRequest webRequest,
+                                  final WebDataBinderFactory binderFactory
+    ) throws Exception {
+        final String authHeader = webRequest.getHeader(AUTHORIZATION);
+        final Map<String, Claim> claims = jwtDecoder.parseJwtToken(authHeader);
+        final String email = claims.get("email").asString();
+        final Runner foundRunner = oauthRunnerRepository.joinByMemberEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("여기도 고쳐"));
+
+        return foundRunner;
+    }
+}
