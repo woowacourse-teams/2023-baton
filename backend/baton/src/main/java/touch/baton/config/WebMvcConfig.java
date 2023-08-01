ed13e4c114208452c5a0f2ed8f2a97a34bb75361
Jeonghoon Park <39729721+shb03323@users.noreply.github.com>
2023-08-01 13:29:01 +0900

    CORS 환경별로 allowed origin 변경 (#169)
    * feat: submodule 업데이트
    
    * feat: gitignore 에 application.yml 추가
    
    * feat: application.yml 중요 환경 변수 숨기기

--- backend/baton/src/main/java/touch/baton/config/WebMvcConfig.java
+++ backend/baton/src/main/java/touch/baton/config/WebMvcConfig.java
@@ -1,22 +1,23 @@
 package touch.baton.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 import static org.springframework.http.HttpHeaders.LOCATION;
-import static org.springframework.http.HttpMethod.DELETE;
-import static org.springframework.http.HttpMethod.GET;
-import static org.springframework.http.HttpMethod.OPTIONS;
-import static org.springframework.http.HttpMethod.PATCH;
-import static org.springframework.http.HttpMethod.POST;
-import static org.springframework.http.HttpMethod.PUT;
+import static org.springframework.http.HttpMethod.*;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${cors.allowed-origin}")
+    private String allowedOrigin;
+
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(allowedOrigin)
                 .allowCredentials(false)
                 .allowedMethods(GET.name(), POST.name(), PUT.name(), PATCH.name(), DELETE.name(), OPTIONS.name())
                 .exposedHeaders(LOCATION)
