6610e9673144239423e4b2fb6736ca67d9949d4e
hyena <goatsfish@gmail.com>
2023-08-02 17:13:05 +0900

    refactor: JWT 디코더 내부 검증 변경

--- backend/baton/src/main/java/touch/baton/infra/auth/jwt/JwtDecoder.java
+++ backend/baton/src/main/java/touch/baton/infra/auth/jwt/JwtDecoder.java
@@ -3,10 +3,11 @@ package touch.baton.infra.auth.jwt;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.SignatureException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
-import java.util.Objects;
+import touch.baton.domain.common.exception.ClientErrorCode;
+import touch.baton.domain.oauth.exception.OauthRequestException;
 
 @RequiredArgsConstructor
 @Component
@@ -14,20 +15,16 @@ public class JwtDecoder {
 
     private final JwtConfig jwtConfig;
 
-    public Claims parseJwtToken(final String authHeader) {
-        if (Objects.isNull(authHeader)) {
-            throw new IllegalStateException("네 맞습니다. 예외 처리해주세요");
-        }
-        if (!authHeader.startsWith("Bearer ")) {
-            throw new IllegalStateException("네 맞습니다. 예외 처리해주세요");
-        }
+    public Claims parseJwtToken(final String token) {
+        try {
+            final JwtParser jwtParser = Jwts.parserBuilder()
+                    .setSigningKey(jwtConfig.getSecretKey())
+                    .requireIssuer(jwtConfig.getIssuer())
+                    .build();
 
-        final String token = authHeader.substring("Bearer ".length());
-        final JwtParser jwtParser = Jwts.parserBuilder()
-                .setSigningKey(jwtConfig.getSecretKey())
-                .requireIssuer(jwtConfig.getIssuer())
-                .build();
-
-        return jwtParser.parseClaimsJws(token).getBody();
+            return jwtParser.parseClaimsJws(token).getBody();
+        } catch (SignatureException e) {
+            throw new OauthRequestException(ClientErrorCode.JWT_SIGNATURE_IS_WRONG);
+        }
     }
 }
