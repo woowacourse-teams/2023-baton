61d83a63f0f7722f9bbbdb9f1dc16765762deb1b
Ethan <kim76744dev@gmail.com>
2023-08-02 13:35:11 +0900

    feat: Description VO 구현

---
+++ backend/baton/src/main/java/touch/baton/domain/common/vo/Description.java
@@ -0,0 +1,34 @@
+package touch.baton.domain.common.vo;
+
+import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.util.Objects;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@Embeddable
+public class Description {
+
+    private static final String INITIAL_DESCRIPTION = "";
+
+    @ColumnDefault(INITIAL_DESCRIPTION)
+    private String value;
+
+    public Description(final String value) {
+        validateNotNull(value);
+        this.value = value;
+    }
+
+    private void validateNotNull(final String value) {
+        if (Objects.isNull(value)) {
+            throw new IllegalArgumentException("Description 객체 내부에 value 는 null 일 수 없습니다.");
+        }
+    }
+}
