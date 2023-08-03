061844256950aeb36337322279259f721d90cc86
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-08-03 14:41:01 +0900

    Oauth API prefix 수정 및 깃허브 소셜 로그인 Http Method 수정 (#193)
    * fix: Oauth API prefix 수정 및 깃허브 소셜 로그인 Http Method 수정
    
    * test: Oauth Api Test prefix 수정

--- backend/baton/src/main/java/touch/baton/domain/oauth/controller/OauthController.java
+++ backend/baton/src/main/java/touch/baton/domain/oauth/controller/OauthController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +17,7 @@ import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.HttpStatus.FOUND;
 
 @RequiredArgsConstructor
-@RequestMapping("/oauth")
+@RequestMapping("/api/v1/oauth")
 @RestController
 public class OauthController {
 
@@ -34,7 +33,7 @@ public class OauthController {
         return ResponseEntity.status(FOUND).build();
     }
 
-    @PostMapping("/login/{oauthType}")
+    @GetMapping("/login/{oauthType}")
     public ResponseEntity<Void> login(@PathVariable final OauthType oauthType,
                                       @RequestParam final String code
     ) {
