fad96321aad61720863fc98771f7aa586026ae8a
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-08-03 13:25:10 +0900

    Github Social Login 4차 구현 (#164)
    * chore: HttpInterface 사용을 위한 의존성 추가
    
    * feat: 소셜 타입 구분을 위한 OauthType 추가
    
    * feat: AuthCode 를 받아오기 위한 인터페이스와 확장성을 고려한 컴포시트 패턴 추가
    
    * feat: Oauth 정보를 받아올 인터페이스와 확장성을 위한 컴포시트 패턴 구현
    
    * feat: 외부 변수를 받아올 GithubOauthConfig 구현 및 최상단 애플리케이션에 읽어올 수 있도록 스캔 어노테이션 적용
    
    * feat: Github Oauth 요청을 위한 구현체 및 스프링 컨테이너에 빈 등록을 위한 HttpInterfaceConfig 설정 구현
    
    * feat: Oauth 로그인을 받을 컨트롤러와 저장 및 조회를 위한 Oauth 레포지터리, Oauth 서비스 구현
    
    * chore: gradle 에 auth0 jwt 의존성 추가
    
    * feat: AccessToken dto 구현 추가 및 OauthInformation 내부 회사 정보 삭제
    
    * feat: OauthType enum 컨버터 구현
    
    * feat: Jwt 인코더, Jwt 디코더 구현
    
    * feat: Oauth 전용 Runner, Supporter 레포지터리 구현
    
    * feat: Runner, Supporter Principal 어노테이션 및 Runner, Supporter ArgumentResolver 구현
    
    * feat: Oauth 서비스 Jwt 인코드 기능 및 Member, Runner, Supporter 신규 사용자 자동 회원가입 추가, Oauth 컨트롤러 Jwt 헤더 반환 구현
    
    * test: WebMvcTest 시 bean 스캔 문제 해결을 위한 커스텀 MockMvcTest 어노테이션 구현 및 RestdocsConfig 수정
    
    * test: Oauth Api 테스트 및 기존 Api Test 를 MockMvcTest 로 수정
    
    * refactor: auth0 의존성을 삭제하고 jjwt 추가 후 HS256 를 이용하도록 JWT 설정 변경
    
    * refactor: AccessToken JWT 내부 Claim 에서 삭제
    
    * refactor: oauth 패키지 이동
    
    * refactor: JWT 디코더 내부 검증 변경
    
    * refactor: Oauth 사용자 정의 예외로 변경
    
    * chore: 서브 모듈 최신화
    
    * fix: 외부 환경 변수 자바 객체 주입을 문자열로 수정
    
    * refactor: cors origin 외부 환경 변수 주입하도록 변경
    
    * refactor: JWT Signature 클라이언트 요청 예외 에러 코드 변경
    
    * refactor: Oauth Email 에러 코드 JWT 로 변경
    
    * feat: Jwt 디코더 에러 코드 추가
    
    * style: 문자열 상수화 및 메서드 파라미터 컨벤션 적용
    
    * dev 환경 변수 수정 (#188)
    
    fix: 환경 변수 수정
    
    * refactor: AccessToken 명 SocialToken 으로 변경 및 Auth 관련 패키지 분리
    
    ---------
    
    Co-authored-by: Jeonghoon Park <39729721+shb03323@users.noreply.github.com>

---
+++ backend/baton/src/test/java/touch/baton/config/MockMvcTest.java
@@ -0,0 +1,26 @@
+package touch.baton.config;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.core.annotation.AliasFor;
+import touch.baton.domain.oauth.controller.resolver.AuthRunnerPrincipalArgumentResolver;
+import touch.baton.domain.oauth.controller.resolver.AuthSupporterPrincipalArgumentResolver;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@WebMvcTest(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {
+        ArgumentResolverConfig.class,
+        AuthRunnerPrincipalArgumentResolver.class,
+        AuthSupporterPrincipalArgumentResolver.class
+}))
+public @interface MockMvcTest {
+
+    @AliasFor(annotation = WebMvcTest.class, attribute = "value")
+    Class<?>[] value() default {};
+}
--- backend/baton/src/test/java/touch/baton/config/RestdocsConfig.java
+++ backend/baton/src/test/java/touch/baton/config/RestdocsConfig.java
@@ -3,7 +3,6 @@ package touch.baton.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -21,9 +20,8 @@ import static org.springframework.restdocs.operation.preprocess.Preprocessors.pr
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 
-@ExtendWith(MockitoExtension.class)
-@Import(RestDocsResultConfig.class)
 @ExtendWith(RestDocumentationExtension.class)
+@Import(RestDocsResultConfig.class)
 public abstract class RestdocsConfig {
 
     protected MockMvc mockMvc;
@@ -40,6 +38,7 @@ public abstract class RestdocsConfig {
     ) {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
                 .apply(documentationConfiguration(restDocumentation))
+                .alwaysDo(restDocs)
                 .build();
     }
 }
