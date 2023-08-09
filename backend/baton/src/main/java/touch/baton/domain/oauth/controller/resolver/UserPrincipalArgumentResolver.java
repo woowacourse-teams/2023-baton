9a06e9279637d1523980d2ad0de9e917089b3450
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-08-09 15:06:04 +0900

    Member, Runner, Supporter 별 AuthPrincipal 수정 및 추가 (#291)
    refactor: Member, Runner, Supporter 별 AuthPrincipal 변경

---
+++ backend/baton/src/main/java/touch/baton/domain/oauth/controller/resolver/UserPrincipalArgumentResolver.java
@@ -0,0 +1,61 @@
+package touch.baton.domain.oauth.controller.resolver;
+
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import touch.baton.domain.common.exception.ClientErrorCode;
+import touch.baton.domain.oauth.exception.OauthRequestException;
+import touch.baton.infra.auth.jwt.JwtDecoder;
+
+import java.util.Objects;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@RequiredArgsConstructor
+public abstract class UserPrincipalArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private static final String BEARER = "Bearer ";
+
+    private final JwtDecoder jwtDecoder;
+
+    @Override
+    public Object resolveArgument(final MethodParameter parameter,
+                                  final ModelAndViewContainer mavContainer,
+                                  final NativeWebRequest webRequest,
+                                  final WebDataBinderFactory binderFactory
+    ) {
+        final boolean isAuthorizationRequired = isAuthorizationRequired(parameter);
+        final boolean isAuthorizationHeaderNotExist = isAuthorizationHeaderNotExist(webRequest);
+        if (isAuthorizationRequired && isAuthorizationHeaderNotExist) {
+            throw new OauthRequestException(ClientErrorCode.OAUTH_AUTHORIZATION_VALUE_IS_NULL);
+        }
+        if (!isAuthorizationRequired && isAuthorizationHeaderNotExist) {
+            return getGuest();
+        }
+
+        final String authHeader = webRequest.getHeader(AUTHORIZATION);
+        if (!authHeader.startsWith(BEARER)) {
+            throw new OauthRequestException(ClientErrorCode.OAUTH_AUTHORIZATION_BEARER_TYPE_NOT_FOUND);
+        }
+
+        final String token = authHeader.substring(BEARER.length());
+        final Claims claims = jwtDecoder.parseJwtToken(token);
+        final String socialId = claims.get("socialId", String.class);
+
+        return getUser(socialId);
+    }
+
+    private boolean isAuthorizationHeaderNotExist(final NativeWebRequest webRequest) {
+        return Objects.isNull(webRequest.getHeader(AUTHORIZATION));
+    }
+
+    protected abstract boolean isAuthorizationRequired(final MethodParameter parameter);
+
+    protected abstract Object getGuest();
+
+    protected abstract Object getUser(final String socialId);
+}
--- backend/baton/src/main/java/touch/baton/domain/oauth/repository/OauthRunnerRepository.java
+++ backend/baton/src/main/java/touch/baton/domain/oauth/repository/OauthRunnerRepository.java
@@ -3,6 +3,7 @@ package touch.baton.domain.oauth.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import touch.baton.domain.member.vo.SocialId;
 import touch.baton.domain.runner.Runner;
 
 import java.util.Optional;
@@ -13,7 +14,7 @@ public interface OauthRunnerRepository extends JpaRepository<Runner, Long> {
         select r, r.member
         from Runner r
         join fetch Member m on m.id = r.member.id
-        where m.socialId.value = :socialId
+        where m.socialId = :socialId
         """)
-    Optional<Runner> joinByMemberSocialId(@Param("socialId") final String socialId);
+    Optional<Runner> joinByMemberSocialId(@Param("socialId") final SocialId socialId);
 }
