5054d08ad01bd9231b6786bc057e2b6c6e90e6ce
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-08-09 16:03:02 +0900

    서포터 프로필 조회 API 구현 (#298)
    * test: 인수 테스트 Jwt Mocking 추가
    
    * feat: 서포터 프로필 응답 객체 추가
    
    * test: 서포터 프로필 조회 인수 테스트 추가
    
    * feat: 서포터와 사용자 패치 조인 기능 추가
    
    * feat: 서포터 프로필 조회 서비스 기능 추가
    
    * feat: 서포터 프로필 조회 API 추가

--- backend/baton/src/main/java/touch/baton/domain/supporter/service/SupporterService.java
+++ backend/baton/src/main/java/touch/baton/domain/supporter/service/SupporterService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import touch.baton.domain.supporter.Supporter;
+import touch.baton.domain.supporter.exception.SupporterBusinessException;
 import touch.baton.domain.supporter.repository.SupporterRepository;
 
 import java.util.List;
@@ -17,5 +18,10 @@ public class SupporterService {
 
     public List<Supporter> readAllSupporters() {
         return supporterRepository.findAll();
+    }
+
+    public Supporter readBySupporterId(final Long supporterId) {
+        return supporterRepository.joinMemberBySupporterId(supporterId)
+                .orElseThrow(() -> new SupporterBusinessException("존재하지 않는 서포터 식별자값으로 조회할 수 없습니다."));
     }
 }
