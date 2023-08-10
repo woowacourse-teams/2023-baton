dbc8b4a1e19578b363cc82597c3d50b02473be6e
ditoo <shb03323@naver.com>
2023-08-10 19:28:03 +0900

    refactor: supporter 와 technicalTag 로 supporterTechnicalTag 찾는 기능 제거

--- backend/baton/src/main/java/touch/baton/domain/technicaltag/repository/SupporterTechnicalTagRepository.java
+++ backend/baton/src/main/java/touch/baton/domain/technicaltag/repository/SupporterTechnicalTagRepository.java
@@ -1,13 +1,7 @@
 package touch.baton.domain.technicaltag.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import touch.baton.domain.supporter.Supporter;
 import touch.baton.domain.technicaltag.SupporterTechnicalTag;
-import touch.baton.domain.technicaltag.TechnicalTag;
-
-import java.util.Optional;
 
 public interface SupporterTechnicalTagRepository extends JpaRepository<SupporterTechnicalTag, Long> {
-
-    Optional<SupporterTechnicalTag> findBySupporterAndTechnicalTag(final Supporter supporter, final TechnicalTag technicalTag);
 }
