b08f0ecc4f503532a4c562cd4186c655a7d8afaa
Ethan <kim76744dev@gmail.com>
2023-08-10 17:31:08 +0900

    refactor: RestAssured 테스트 변수 한글로 변경

--- backend/baton/src/test/java/touch/baton/assure/runner/RunnerProfileControllerTest.java
+++ backend/baton/src/test/java/touch/baton/assure/runner/RunnerProfileControllerTest.java
@@ -19,14 +19,14 @@ class RunnerProfileControllerTest extends AssuredTestConfig {
     @Test
     void 러너_프로필_조회에_성공한다() {
         // given
-        final Member member = memberRepository.save(MemberFixture.createEthan());
-        final TechnicalTag javaTag = technicalTagRepository.save(TechnicalTagFixture.createJava());
-        final TechnicalTag reactTag = technicalTagRepository.save(TechnicalTagFixture.createReact());
-        final Runner savedRunner = runnerRepository.save(RunnerFixture.createRunner(member, List.of(javaTag, reactTag)));
+        final Member 사용자_에단 = memberRepository.save(MemberFixture.createEthan());
+        final TechnicalTag 자바_태그 = technicalTagRepository.save(TechnicalTagFixture.createJava());
+        final TechnicalTag 리액트_태그 = technicalTagRepository.save(TechnicalTagFixture.createReact());
+        final Runner 러너_에단 = runnerRepository.save(RunnerFixture.createRunner(사용자_에단, List.of(자바_태그, 리액트_태그)));
 
         // when, then
         RunnerProfileAssuredSupport
-                .클라이언트_요청().러너_피드백을_상세_조회한다(savedRunner.getId())
+                .클라이언트_요청().러너_피드백을_상세_조회한다(러너_에단.getId())
                 .서버_응답().러너_프로필_상세_조회를_검증한다(new RunnerProfileResponse.Detail(
                         1L,
                         "에단",
