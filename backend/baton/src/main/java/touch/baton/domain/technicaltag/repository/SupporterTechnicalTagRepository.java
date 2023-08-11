cd43f096dfb63c5619d1928b61e1652e0ff47b01
ditoo <shb03323@naver.com>
2023-08-11 13:22:16 +0900

    feat: 서포터로 SupporterTechnicalTag 제거 기능 구현

--- backend/baton/src/main/java/touch/baton/domain/technicaltag/repository/SupporterTechnicalTagRepository.java
+++ backend/baton/src/main/java/touch/baton/domain/technicaltag/repository/SupporterTechnicalTagRepository.java
@@ -1,7 +1,17 @@
 package touch.baton.domain.technicaltag.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import touch.baton.domain.supporter.Supporter;
 import touch.baton.domain.technicaltag.SupporterTechnicalTag;
 
 public interface SupporterTechnicalTagRepository extends JpaRepository<SupporterTechnicalTag, Long> {
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM SupporterTechnicalTag st WHERE st.supporter = :supporter")
+    int deleteBySupporter(@Param("supporter") final Supporter supporter);
 }
