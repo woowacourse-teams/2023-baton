565733931c29635f5f9c9bf7bab5d4ca58b35d94
ditoo <shb03323@naver.com>
2023-08-13 02:07:56 +0900

    feat: repository 구현

--- backend/baton/src/main/java/touch/baton/domain/supporter/repository/SupporterRunnerPostRepository.java
+++ backend/baton/src/main/java/touch/baton/domain/supporter/repository/SupporterRunnerPostRepository.java
@@ -1,7 +1,10 @@
 package touch.baton.domain.supporter.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import touch.baton.domain.supporter.Supporter;
 import touch.baton.domain.supporter.SupporterRunnerPost;
 
 public interface SupporterRunnerPostRepository extends JpaRepository<SupporterRunnerPost, Long> {
+
+    void deleteBySupporterAndRunnerPostId(final Supporter supporter, final Long runnerPostId);
 }
---
+++ backend/baton/src/test/java/touch/baton/domain/supporter/repository/SupporterRunnerPostRepositoryTest.java
@@ -0,0 +1,58 @@
+package touch.baton.domain.supporter.repository;
+
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import touch.baton.config.RepositoryTestConfig;
+import touch.baton.domain.member.Member;
+import touch.baton.domain.runner.Runner;
+import touch.baton.domain.runnerpost.RunnerPost;
+import touch.baton.domain.runnerpost.vo.Deadline;
+import touch.baton.domain.supporter.Supporter;
+import touch.baton.domain.supporter.SupporterRunnerPost;
+import touch.baton.fixture.domain.MemberFixture;
+import touch.baton.fixture.domain.RunnerFixture;
+import touch.baton.fixture.domain.RunnerPostFixture;
+import touch.baton.fixture.domain.SupporterFixture;
+import touch.baton.fixture.domain.SupporterRunnerPostFixture;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SupporterRunnerPostRepositoryTest extends RepositoryTestConfig {
+
+    @Autowired
+    private SupporterRunnerPostRepository supporterRunnerPostRepository;
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @DisplayName("서포터의_러너_게시글_제안을_철회하는데_성공한다")
+    @Test
+    void deleteBySupporterAndRunnerPostId() {
+        // given
+        final Member reviewerMember = MemberFixture.createDitoo();
+        entityManager.persist(reviewerMember);
+        final Supporter reviewerSupporter = SupporterFixture.create(reviewerMember);
+        entityManager.persist(reviewerSupporter);
+
+        final Member revieweeMember = MemberFixture.createJudy();
+        entityManager.persist(revieweeMember);
+        final Runner revieweeRunner = RunnerFixture.createRunner(revieweeMember);
+        entityManager.persist(revieweeRunner);
+        final RunnerPost runnerPost = RunnerPostFixture.create(revieweeRunner, reviewerSupporter, new Deadline(LocalDateTime.now().plusHours(100)));
+        entityManager.persist(runnerPost);
+
+        final SupporterRunnerPost deletedSupporterRunnerPost = SupporterRunnerPostFixture.create(reviewerSupporter, runnerPost);
+        entityManager.persist(deletedSupporterRunnerPost);
+
+        // when
+        supporterRunnerPostRepository.deleteBySupporterAndRunnerPostId(reviewerSupporter, runnerPost.getId());
+        entityManager.flush();
+
+        // then
+        assertThat(supporterRunnerPostRepository.findById(deletedSupporterRunnerPost.getId())).isNotPresent();
+    }
+}
