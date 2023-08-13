568d7d8e8f3fcfe5a8a5d7cf97025953ea512e9a
eunbii0213 <eunbii0213@naver.com>
2023-08-13 14:35:24 +0900

    test: service 테스트에서 Response 를 의존하지 않도록 작성

--- backend/baton/src/test/java/touch/baton/domain/runner/service/RunnerServiceUpdateTest.java
+++ backend/baton/src/test/java/touch/baton/domain/runner/service/RunnerServiceUpdateTest.java
@@ -1,18 +1,18 @@
 package touch.baton.domain.runner.service;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import touch.baton.config.ServiceTestConfig;
+import touch.baton.domain.common.vo.TagName;
 import touch.baton.domain.member.Member;
 import touch.baton.domain.runner.Runner;
-import touch.baton.domain.runner.controller.response.RunnerResponse;
 import touch.baton.domain.runner.service.dto.RunnerUpdateRequest;
 import touch.baton.fixture.domain.MemberFixture;
 import touch.baton.fixture.domain.RunnerFixture;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
@@ -35,7 +35,6 @@ class RunnerServiceUpdateTest extends ServiceTestConfig {
         final Runner runnerJudy = runnerRepository.save(RunnerFixture.createRunner(memberJudy));
         final RunnerUpdateRequest runnerUpdateRequest = new RunnerUpdateRequest("변경된 이름", "변경된 회사", "변경된 자기소개", List.of("changedTag1", "changedTag2"));
 
-        runnerService.updateRunner(runnerJudy, runnerUpdateRequest);
         // when, then
         assertThatCode(() -> runnerService.updateRunner(runnerJudy, runnerUpdateRequest))
                 .doesNotThrowAnyException();
@@ -50,15 +49,21 @@ class RunnerServiceUpdateTest extends ServiceTestConfig {
         final RunnerUpdateRequest runnerUpdateRequest = new RunnerUpdateRequest("변경된 이름", "변경된 회사", "변경된 자기소개", List.of("changedTag1", "changedTag2"));
 
         runnerService.updateRunner(runnerJudy, runnerUpdateRequest);
-        final Optional<Runner> foundJudy = runnerRepository.findById(runnerJudy.getId());
-        final RunnerResponse.MyProfile judyProfile = RunnerResponse.MyProfile.from(foundJudy.get());
+        final Runner foundRunnerJudy = runnerRepository.findById(runnerJudy.getId()).get();
 
         // when, then
         assertAll(
-                () -> assertThat(judyProfile.name()).isEqualTo(runnerUpdateRequest.name()),
-                () -> assertThat(judyProfile.company()).isEqualTo(runnerUpdateRequest.company()),
-                () -> assertThat(judyProfile.introduction()).isEqualTo(runnerUpdateRequest.introduction()),
-                () -> assertThat(judyProfile.technicalTags()).isEqualTo(runnerUpdateRequest.technicalTags())
+                () -> assertThat(foundRunnerJudy.getMember().getMemberName().getValue()).isEqualTo(runnerUpdateRequest.name()),
+                () -> assertThat(foundRunnerJudy.getMember().getCompany().getValue()).isEqualTo(runnerUpdateRequest.company()),
+                () -> assertThat(foundRunnerJudy.getIntroduction().getValue()).isEqualTo(runnerUpdateRequest.introduction()),
+                () -> assertThat(getRunnerTechnicalTags(foundRunnerJudy)).isEqualTo(runnerUpdateRequest.technicalTags())
         );
+    }
+
+    @NotNull
+    private List<String> getRunnerTechnicalTags(final Runner foundRunnerJudy) {
+        return foundRunnerJudy.getRunnerTechnicalTags().getRunnerTechnicalTags().stream()
+                .map(runnerTechnicalTag -> runnerTechnicalTag.getTechnicalTag().getTagName().getValue())
+                .toList();
     }
 }
