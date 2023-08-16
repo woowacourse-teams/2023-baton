a14cf4dc92677f48dc219af6affa4210b3ea766e
ditoo <shb03323@naver.com>
2023-08-16 16:35:08 +0900

    feat: aop 로깅 적용

---
+++ backend/baton/src/main/java/touch/baton/config/filter/MDCLoggingFilter.java
@@ -0,0 +1,27 @@
+package touch.baton.config.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import org.apache.logging.log4j.core.config.Order;
+import org.slf4j.MDC;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+class MDCLoggingFilter implements Filter {
+
+    @Override
+    public void doFilter(final ServletRequest servletRequest, final ServletResponse servletResponse, final FilterChain filterChain) throws IOException, ServletException {
+        final UUID uuid = UUID.randomUUID();
+        MDC.put("request_id", uuid.toString());
+        filterChain.doFilter(servletRequest, servletResponse);
+        MDC.clear();
+    }
+}
--- backend/baton/src/main/java/touch/baton/domain/common/GlobalControllerAdvice.java
+++ backend/baton/src/main/java/touch/baton/domain/common/GlobalControllerAdvice.java
@@ -1,16 +1,87 @@
 package touch.baton.domain.common;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.TypeMismatchException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.web.ErrorResponseException;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import touch.baton.common.LoggerUtils;
+import touch.baton.domain.common.exception.BaseException;
 import touch.baton.domain.common.exception.ClientRequestException;
 import touch.baton.domain.common.response.ErrorResponse;
+import touch.baton.domain.common.response.ServerErrorResponse;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalControllerAdvice {
 
     @ExceptionHandler(ClientRequestException.class)
-    public ResponseEntity<ErrorResponse> handleClientRequest(ClientRequestException e) {
-        return ResponseEntity.status(e.getErrorCode().getHttpStatus()).body(ErrorResponse.from(e));
+    public ResponseEntity<ErrorResponse> handleClientRequest(final HttpServletRequest request,
+                                                             final ClientRequestException e
+    ) {
+        LoggerUtils.logWarn(request, e);
+        return ResponseEntity
+                .status(e.getErrorCode().getHttpStatus())
+                .body(ErrorResponse.from(e));
+    }
+
+    @ExceptionHandler({HttpRequestMethodNotSupportedException.class,
+            HttpMediaTypeNotSupportedException.class,
+            HttpMediaTypeNotAcceptableException.class,
+            MissingPathVariableException.class,
+            MissingServletRequestParameterException.class,
+            ServletRequestBindingException.class,
+            MethodArgumentNotValidException.class,
+            NoHandlerFoundException.class,
+            AsyncRequestTimeoutException.class,
+            ErrorResponseException.class,
+            TypeMismatchException.class,
+            HttpMessageNotReadableException.class,
+            HttpMessageNotWritableException.class})
+    public ResponseEntity<ServerErrorResponse> handle(final RuntimeException ex,
+                                                      final HttpHeaders headers,
+                                                      final HttpStatusCode statusCode,
+                                                      final WebRequest request
+    ) {
+        return ResponseEntity.badRequest().body(ServerErrorResponse.from(ex));
+    }
+
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<ServerErrorResponse> handleBaseException(final HttpServletRequest httpServletRequest,
+                                                                   final BaseException ex,
+                                                                   final HttpHeaders headers,
+                                                                   final HttpStatusCode statusCode,
+                                                                   final WebRequest request
+    ) {
+        LoggerUtils.logWarn(httpServletRequest, ex);
+        return ResponseEntity.internalServerError().body(ServerErrorResponse.from(ex));
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<Object> handleExceptionInternal(final Exception ex,
+                                                             final Object body,
+                                                             final HttpHeaders headers,
+                                                             final HttpStatusCode statusCode,
+                                                             final WebRequest request,
+                                                             final HttpServletRequest httpServletRequest
+    ) {
+        LoggerUtils.logError(httpServletRequest, ex);
+        return ResponseEntity.internalServerError().body(ServerErrorResponse.unExpected());
     }
 }
