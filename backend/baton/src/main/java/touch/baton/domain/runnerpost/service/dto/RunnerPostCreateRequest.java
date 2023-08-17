a64c2948fb4742e740494f57bef9455e8d0ff1fc
Ethan <kim76744dev@gmail.com>
2023-08-17 18:52:38 +0900

    refactor: ClientErrorCode static import하도록 변경

--- backend/baton/src/main/java/touch/baton/domain/runnerpost/service/dto/RunnerPostCreateRequest.java
+++ backend/baton/src/main/java/touch/baton/domain/runnerpost/service/dto/RunnerPostCreateRequest.java
@@ -1,6 +1,5 @@
 package touch.baton.domain.runnerpost.service.dto;
 
-import touch.baton.domain.common.exception.ClientErrorCode;
 import touch.baton.domain.common.exception.validator.ValidNotNull;
 import touch.baton.domain.runnerpost.exception.validator.ValidFuture;
 import touch.baton.domain.runnerpost.exception.validator.ValidMaxLength;
@@ -9,18 +8,27 @@ import touch.baton.domain.runnerpost.exception.validator.ValidNotUrl;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public record RunnerPostCreateRequest(@ValidNotNull(clientErrorCode = ClientErrorCode.TITLE_IS_NULL)
+import static touch.baton.domain.common.exception.ClientErrorCode.CONTENTS_ARE_NULL;
+import static touch.baton.domain.common.exception.ClientErrorCode.CONTENTS_OVERFLOW;
+import static touch.baton.domain.common.exception.ClientErrorCode.DEADLINE_IS_NULL;
+import static touch.baton.domain.common.exception.ClientErrorCode.PAST_DEADLINE;
+import static touch.baton.domain.common.exception.ClientErrorCode.PULL_REQUEST_URL_IS_NOT_URL;
+import static touch.baton.domain.common.exception.ClientErrorCode.PULL_REQUEST_URL_IS_NULL;
+import static touch.baton.domain.common.exception.ClientErrorCode.TAGS_ARE_NULL;
+import static touch.baton.domain.common.exception.ClientErrorCode.TITLE_IS_NULL;
+
+public record RunnerPostCreateRequest(@ValidNotNull(clientErrorCode = TITLE_IS_NULL)
                                       String title,
-                                      @ValidNotNull(clientErrorCode = ClientErrorCode.TAGS_ARE_NULL)
+                                      @ValidNotNull(clientErrorCode = TAGS_ARE_NULL)
                                       List<String> tags,
-                                      @ValidNotNull(clientErrorCode = ClientErrorCode.PULL_REQUEST_URL_IS_NULL)
-                                      @ValidNotUrl(clientErrorCode = ClientErrorCode.PULL_REQUEST_URL_IS_NOT_URL)
+                                      @ValidNotNull(clientErrorCode = PULL_REQUEST_URL_IS_NULL)
+                                      @ValidNotUrl(clientErrorCode = PULL_REQUEST_URL_IS_NOT_URL)
                                       String pullRequestUrl,
-                                      @ValidNotNull(clientErrorCode = ClientErrorCode.DEADLINE_IS_NULL)
-                                      @ValidFuture(clientErrorCode = ClientErrorCode.PAST_DEADLINE)
+                                      @ValidNotNull(clientErrorCode = DEADLINE_IS_NULL)
+                                      @ValidFuture(clientErrorCode = PAST_DEADLINE)
                                       LocalDateTime deadline,
-                                      @ValidNotNull(clientErrorCode = ClientErrorCode.CONTENTS_ARE_NULL)
-                                      @ValidMaxLength(clientErrorCode = ClientErrorCode.CONTENTS_OVERFLOW, max = 1000)
+                                      @ValidNotNull(clientErrorCode = CONTENTS_ARE_NULL)
+                                      @ValidMaxLength(clientErrorCode = CONTENTS_OVERFLOW, max = 1000)
                                       String contents
 ) {
 }
