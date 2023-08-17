b9705dc117216cf67f1d016c678cf7009bd53a94
Ethan <kim76744dev@gmail.com>
2023-08-17 22:36:35 +0900

    test: 중복된 테스트 삭제

--- backend/baton/src/test/java/touch/baton/document/runnerpost/read/RunnerPostReadAllApiTest.java
+++ backend/baton/src/test/java/touch/baton/document/runnerpost/read/RunnerPostReadAllApiTest.java
@@ -14,12 +14,10 @@ import touch.baton.domain.runnerpost.controller.RunnerPostController;
 import touch.baton.domain.runnerpost.service.RunnerPostService;
 import touch.baton.domain.runnerpost.vo.Deadline;
 import touch.baton.domain.runnerpost.vo.ReviewStatus;
-import touch.baton.domain.supporter.Supporter;
 import touch.baton.domain.tag.Tag;
 import touch.baton.fixture.domain.MemberFixture;
 import touch.baton.fixture.domain.RunnerFixture;
 import touch.baton.fixture.domain.RunnerPostFixture;
-import touch.baton.fixture.domain.SupporterFixture;
 import touch.baton.fixture.domain.TagFixture;
 
 import java.time.LocalDateTime;
@@ -38,7 +36,6 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.head;
 import static org.springframework.restdocs.payload.JsonFieldType.ARRAY;
 import static org.springframework.restdocs.payload.JsonFieldType.BOOLEAN;
 import static org.springframework.restdocs.payload.JsonFieldType.NUMBER;
@@ -49,10 +46,7 @@ import static org.springframework.restdocs.request.RequestDocumentation.paramete
 import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static touch.baton.fixture.domain.TechnicalTagFixture.createJava;
-import static touch.baton.fixture.domain.TechnicalTagFixture.createSpring;
 import static touch.baton.fixture.vo.DeadlineFixture.deadline;
-import static touch.baton.fixture.vo.ReviewCountFixture.reviewCount;
 import static touch.baton.fixture.vo.TagNameFixture.tagName;
 
 @WebMvcTest(RunnerPostController.class)
@@ -113,68 +107,6 @@ class RunnerPostReadAllApiTest extends RestdocsConfig {
                                 fieldWithPath("pageInfo.totalElements").type(NUMBER).description("총 데이터 수"),
                                 fieldWithPath("pageInfo.currentPage").type(NUMBER).description("현재 페이지"),
                                 fieldWithPath("pageInfo.currentSize").type(NUMBER).description("현재 페이지 데이터 수")
-                        ))
-                );
-    }
-
-    @DisplayName("서포터와 연관된 러너 게시글 페이징 조회 API")
-    @Test
-    void readReferencedBySupporter() throws Exception {
-        // given
-        final Runner runnerJudy = RunnerFixture.createRunner(MemberFixture.createJudy());
-        final Supporter supporterHyena = SupporterFixture.create(reviewCount(10), MemberFixture.createHyena(), List.of(createJava(), createSpring()));
-
-        final Tag javaTag = TagFixture.create(tagName("자바"));
-        final Deadline deadline = deadline(LocalDateTime.now().plusHours(100));
-        final RunnerPost runnerPost = RunnerPostFixture.create(runnerJudy, deadline, List.of(javaTag));
-        runnerPost.assignSupporter(supporterHyena);
-
-        // when
-        final RunnerPost spyRunnerPost = spy(runnerPost);
-        final Supporter spySupporterHyena = spy(supporterHyena);
-        when(spySupporterHyena.getId()).thenReturn(1L);
-        when(spyRunnerPost.getId()).thenReturn(1L);
-
-        final List<RunnerPost> runnerPosts = List.of(spyRunnerPost);
-        final PageRequest pageOne = PageRequest.of(1, 10);
-        final PageImpl<RunnerPost> pageRunnerPosts = new PageImpl<>(runnerPosts, pageOne, runnerPosts.size());
-        when(runnerPostService.readRunnerPostsBySupporterIdAndReviewStatus(any(), any(), any()))
-                .thenReturn(pageRunnerPosts);
-        when(runnerPostService.readCountsByRunnerPostIds(anyList()))
-                .thenReturn(List.of(1L));
-
-        // then
-        mockMvc.perform(get("/api/v1/posts/runner/search")
-                        .characterEncoding(UTF_8)
-                        .contentType(APPLICATION_JSON)
-                        .queryParam("size", String.valueOf(pageOne.getPageSize()))
-                        .queryParam("page", String.valueOf(pageOne.getPageNumber()))
-                        .queryParam("supporterId", String.valueOf(spySupporterHyena.getId()))
-                        .queryParam("reviewStatus", ReviewStatus.IN_PROGRESS.name()))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(APPLICATION_JSON))
-                .andDo(restDocs.document(
-                        queryParameters(
-                                parameterWithName("size").description("페이지 사이즈"),
-                                parameterWithName("page").description("페이지 번호"),
-                                parameterWithName("supporterId").description("서포터 식별자값"),
-                                parameterWithName("reviewStatus").description("리뷰 상태")
-                        ),
-                        responseFields(
-                                fieldWithPath("pageInfo.isFirst").type(BOOLEAN).description("첫 번째 페이지인지"),
-                                fieldWithPath("pageInfo.isLast").type(BOOLEAN).description("마지막 페이지 인지"),
-                                fieldWithPath("pageInfo.hasNext").type(BOOLEAN).description("다음 페이지가 있는지"),
-                                fieldWithPath("pageInfo.totalPages").type(NUMBER).description("총 페이지 수"),
-                                fieldWithPath("pageInfo.totalElements").type(NUMBER).description("총 데이터 수"),
-                                fieldWithPath("pageInfo.currentPage").type(NUMBER).description("현재 페이지 번호"),
-                                fieldWithPath("pageInfo.currentSize").type(NUMBER).description("현재 페이지 데이터 수"),
-                                fieldWithPath("data.[].runnerPostId").type(NUMBER).description("러너 게시글 식별자값(id)"),
-                                fieldWithPath("data.[].title").type(STRING).description("러너 게시글 제목"),
-                                fieldWithPath("data.[].deadline").type(STRING).description("러너 게시글의 마감 기한"),
-                                fieldWithPath("data.[].tags").type(ARRAY).description("러너 게시글 태그 목록"),
-                                fieldWithPath("data.[].watchedCount").type(NUMBER).description("러너 게시글의 조회수"),
-                                fieldWithPath("data.[].applicantCount").type(NUMBER).description("러너 게시글에 신청한 서포터 수"),
-                                fieldWithPath("data.[].reviewStatus").type(STRING).description("러너 게시글 리뷰 상태")
                         ))
                 );
     }
