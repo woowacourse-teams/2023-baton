0164812d34487208104316f9c8169a633dbab78c
ditoo <shb03323@naver.com>
2023-08-17 22:10:41 +0900

    test: 필요 없는 테스트 삭제

--- backend/baton/src/test/java/touch/baton/assure/runnerpost/RunnerPostAssuredCreateSupport.java
+++ backend/baton/src/test/java/touch/baton/assure/runnerpost/RunnerPostAssuredCreateSupport.java
@@ -14,6 +14,7 @@ import java.util.Map;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 import static org.springframework.http.HttpHeaders.LOCATION;
 
+@SuppressWarnings("NonAsciiCharacters")
 public class RunnerPostAssuredCreateSupport {
 
     private RunnerPostAssuredCreateSupport() {
