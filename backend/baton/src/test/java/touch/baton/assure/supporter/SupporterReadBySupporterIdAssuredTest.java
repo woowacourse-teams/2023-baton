8d65d7a7ebff27e94b9ed98a53c285177f2b4460
hyena <goatsfish@gmail.com>
2023-09-01 16:30:54 +0900

    test: 서포터 조회 인수 테스트 리팩터링

--- backend/baton/src/test/java/touch/baton/assure/supporter/SupporterReadBySupporterIdAssuredTest.java
+++ backend/baton/src/test/java/touch/baton/assure/supporter/SupporterReadBySupporterIdAssuredTest.java
@@ -2,71 +2,50 @@ package touch.baton.assure.supporter;
 
 import org.junit.jupiter.api.Test;
 import touch.baton.config.AssuredTestConfig;
-import touch.baton.domain.member.Member;
+import touch.baton.config.infra.auth.oauth.authcode.MockAuthCodes;
+import touch.baton.domain.member.vo.SocialId;
 import touch.baton.domain.supporter.Supporter;
-import touch.baton.domain.supporter.controller.response.SupporterResponse;
-import touch.baton.domain.technicaltag.TechnicalTag;
-import touch.baton.fixture.domain.MemberFixture;
-import touch.baton.fixture.domain.SupporterFixture;
-import touch.baton.fixture.domain.TechnicalTagFixture;
 
-import java.util.List;
+import java.util.Collections;
 
-import static touch.baton.assure.supporter.SupporterAssuredSupport.서포터_프로필_응답;
+import static touch.baton.assure.supporter.SupporterAssuredSupport.서포터_MyProfile_응답;
+import static touch.baton.assure.supporter.SupporterAssuredSupport.서포터_Profile_응답;
 
 @SuppressWarnings("NonAsciiCharacters")
 class SupporterReadBySupporterIdAssuredTest extends AssuredTestConfig {
 
     @Test
     void 서포터_프로필을_조회한다() {
-        final Member 사용자_헤나 = memberRepository.save(MemberFixture.createHyena());
-        final Supporter 서포터_헤나 = supporterRepository.save(SupporterFixture.create(사용자_헤나));
+        // given
+        final String 헤나_액세스_토큰 = oauthLoginTestManager.소셜_회원가입을_진행한_후_액세스_토큰을_반환한다(MockAuthCodes.hyenaAuthCode());
 
+        final SocialId 헤나_소셜_아이디 = jwtTestManager.parseToSocialId(헤나_액세스_토큰);
+        final Supporter 서포터_헤나 = supporterRepository.getBySocialId(헤나_소셜_아이디);
+
+        // when, then
         SupporterAssuredSupport
                 .클라이언트_요청()
                 .서포터_프로필을_서포터_식별자값으로_조회한다(서포터_헤나.getId())
 
                 .서버_응답()
-                .서포터_프로필_조회_성공을_검증한다(서포터_프로필_응답(서포터_헤나));
+                .서포터_프로필_조회_성공을_검증한다(서포터_Profile_응답(서포터_헤나, Collections.emptyList()));
     }
 
     @Test
     void 서포터_마이페이지_프로필을_조회한다() {
         // given
-        final String 디투_소셜_아이디 = "hongsile";
-        final Member 사용자_디투 = memberRepository.save(MemberFixture.createWithSocialId(디투_소셜_아이디));
+        final String 헤나_액세스_토큰 = oauthLoginTestManager.소셜_회원가입을_진행한_후_액세스_토큰을_반환한다(MockAuthCodes.hyenaAuthCode());
 
-        final TechnicalTag 자바_태그 = technicalTagRepository.save(TechnicalTagFixture.createJava());
-        final TechnicalTag 리액트_태그 = technicalTagRepository.save(TechnicalTagFixture.createReact());
-
-        final Supporter 서포터_디투 = supporterRepository.save(SupporterFixture.create(사용자_디투, List.of(자바_태그, 리액트_태그)));
-        final String 서포터_디투_액세스_토큰 = login(디투_소셜_아이디);
+        final SocialId 헤나_소셜_아이디 = jwtTestManager.parseToSocialId(헤나_액세스_토큰);
+        final Supporter 서포터_헤나 = supporterRepository.getBySocialId(헤나_소셜_아이디);
 
         // when, then
         SupporterAssuredSupport
                 .클라이언트_요청()
-                .액세스_토큰으로_로그인_한다(서포터_디투_액세스_토큰)
+                .액세스_토큰으로_로그인_한다(헤나_액세스_토큰)
                 .서포터_마이페이지를_액세스_토큰으로_조회한다()
 
                 .서버_응답()
-                .서포터_마이페이지_프로필_조회_성공을_검증한다(응답(서포터_디투));
-    }
-
-    private SupporterResponse.MyProfile 응답(final Supporter 서포터) {
-        final Member 사용자_디투 = 서포터.getMember();
-        return new SupporterResponse.MyProfile(
-                사용자_디투.getMemberName().getValue(),
-                사용자_디투.getImageUrl().getValue(),
-                사용자_디투.getGithubUrl().getValue(),
-                서포터.getIntroduction().getValue(),
-                사용자_디투.getCompany().getValue(),
-                서포터_기술_스택(서포터)
-        );
-    }
-
-    private List<String> 서포터_기술_스택(final Supporter 서포터) {
-        return 서포터.getSupporterTechnicalTags().getSupporterTechnicalTags().stream()
-                .map(supporterTechnicalTag -> supporterTechnicalTag.getTechnicalTag().getTagName().getValue())
-                .toList();
+                .서포터_마이페이지_프로필_조회_성공을_검증한다(서포터_MyProfile_응답(서포터_헤나, Collections.emptyList()));
     }
 }
