6807370b816d14ee42b785a24ad34a220a3dc1e1
hyena <goatsfish@gmail.com>
2023-09-01 16:29:52 +0900

    test: 러너 게시글 기타 인수 테스트 리팩터링

--- backend/baton/src/test/java/touch/baton/assure/runnerpost/RunnerPostAssuredSupport.java
+++ backend/baton/src/test/java/touch/baton/assure/runnerpost/RunnerPostAssuredSupport.java
@@ -15,11 +15,14 @@ import touch.baton.domain.common.response.ErrorResponse;
 import touch.baton.domain.common.response.PageResponse;
 import touch.baton.domain.runner.Runner;
 import touch.baton.domain.runner.controller.response.RunnerResponse;
+import touch.baton.domain.runnerpost.RunnerPost;
 import touch.baton.domain.runnerpost.controller.response.RunnerPostResponse;
+import touch.baton.domain.runnerpost.controller.response.SupporterRunnerPostResponse;
 import touch.baton.domain.runnerpost.controller.response.SupporterRunnerPostResponses;
 import touch.baton.domain.runnerpost.service.dto.RunnerPostCreateRequest;
 import touch.baton.domain.runnerpost.service.dto.RunnerPostUpdateRequest;
 import touch.baton.domain.runnerpost.vo.ReviewStatus;
+import touch.baton.domain.supporter.Supporter;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -37,6 +40,10 @@ public class RunnerPostAssuredSupport {
 
     public static RunnerPostClientRequestBuilder 클라이언트_요청() {
         return new RunnerPostClientRequestBuilder();
+    }
+
+    public static RunnerPostUpdateRequest.SelectSupporter 러너의_서포터_선택_요청(final Long 러너가_선택한_서포터_식별자값) {
+        return new RunnerPostUpdateRequest.SelectSupporter(러너가_선택한_서포터_식별자값);
     }
 
     public static RunnerPostResponse.Detail 러너_게시글_Detail_응답(final Long 러너_게시글_식별자값,
@@ -68,8 +75,8 @@ public class RunnerPostAssuredSupport {
         );
     }
 
-    public static PageResponse<RunnerPostResponse.ReferencedBySupporter> 서포터가_리뷰_완료한_러너_게시글_응답(final Pageable 페이징_정보,
-                                                                                               final List<RunnerPostResponse.ReferencedBySupporter> 서포터가_연관된_러너_게시글_목록
+    public static PageResponse<RunnerPostResponse.ReferencedBySupporter> 서포터와_연관된_러너_게시글_페이징_응답(final Pageable 페이징_정보,
+                                                                                                final List<RunnerPostResponse.ReferencedBySupporter> 서포터가_연관된_러너_게시글_목록
     ) {
         final Page<RunnerPostResponse.ReferencedBySupporter> 페이징된_서포터가_연관된_러너_게시글 = new PageImpl<>(서포터가_연관된_러너_게시글_목록, 페이징_정보, 서포터가_연관된_러너_게시글_목록.size());
         final PageResponse<RunnerPostResponse.ReferencedBySupporter> 페이징된_서포터가_연관된_러너_게시글_응답 = PageResponse.from(페이징된_서포터가_연관된_러너_게시글);
@@ -77,13 +84,67 @@ public class RunnerPostAssuredSupport {
         return 페이징된_서포터가_연관된_러너_게시글_응답;
     }
 
-    public static PageResponse<RunnerPostResponse.Simple> 러너_게시글_전체_조회_응답(final Pageable 페이징_정보,
-                                                                          final List<RunnerPostResponse.Simple> 러너_게시글_목록
+    public static RunnerPostResponse.ReferencedBySupporter 서포터와_연관된_러너_게시글_응답(final RunnerPost 러너_게시글,
+                                                                              final List<String> 러너_게시글_태그_목록,
+                                                                              final int 조회수,
+                                                                              final long 러너_게시글에_지원한_서포터수,
+                                                                              final ReviewStatus 리뷰_상태
+    ) {
+        return new RunnerPostResponse.ReferencedBySupporter(
+                러너_게시글.getId(),
+                러너_게시글.getTitle().getValue(),
+                러너_게시글.getDeadline().getValue(),
+                러너_게시글_태그_목록,
+                조회수,
+                러너_게시글에_지원한_서포터수,
+                리뷰_상태.name()
+        );
+    }
+
+    public static RunnerPostResponse.Simple 러너_게시글_Simple_응답(final RunnerPost 러너_게시글,
+                                                             final int 조회수,
+                                                             final long 지원한_서포터_수,
+                                                             final ReviewStatus 리뷰_상태,
+                                                             final List<String> 러너_게시글_태그_목록
+    ) {
+        return new RunnerPostResponse.Simple(
+                러너_게시글.getId(),
+                러너_게시글.getTitle().getValue(),
+                러너_게시글.getDeadline().getValue(),
+                조회수,
+                지원한_서포터_수,
+                리뷰_상태.name(),
+                RunnerResponse.Simple.from(러너_게시글.getRunner()),
+                러너_게시글_태그_목록
+        );
+    }
+
+    public static PageResponse<RunnerPostResponse.Simple> 러너_게시글_전체_Simple_페이징_응답(final Pageable 페이징_정보,
+                                                                                  final List<RunnerPostResponse.Simple> 러너_게시글_목록
     ) {
         final Page<RunnerPostResponse.Simple> 페이징된_러너_게시글 = new PageImpl<>(러너_게시글_목록, 페이징_정보, 러너_게시글_목록.size());
         final PageResponse<RunnerPostResponse.Simple> 페이징된_러너_게시글_응답 = PageResponse.from(페이징된_러너_게시글);
 
         return 페이징된_러너_게시글_응답;
+    }
+
+    public static RunnerPostResponse.SimpleInMyPage 마이페이지_러너_게시글_SimpleInMyPage_응답(final RunnerPost 러너_게시글,
+                                                                                   final Long 지원한_서포터_식별자값,
+                                                                                   final int 조회수,
+                                                                                   final long 지원한_서포터_수,
+                                                                                   final ReviewStatus 리뷰_상태,
+                                                                                   final List<String> 러너_게시글_태그_목록
+    ) {
+        return new RunnerPostResponse.SimpleInMyPage(
+                러너_게시글.getId(),
+                지원한_서포터_식별자값,
+                러너_게시글.getTitle().getValue(),
+                러너_게시글.getDeadline().getValue(),
+                러너_게시글_태그_목록,
+                조회수,
+                지원한_서포터_수,
+                리뷰_상태.name()
+        );
     }
 
     public static PageResponse<RunnerPostResponse.SimpleInMyPage> 마이페이지_러너_게시글_응답(final Pageable 페이징_정보,
@@ -93,6 +154,26 @@ public class RunnerPostAssuredSupport {
         final PageResponse<RunnerPostResponse.SimpleInMyPage> 페이징된_마이페이지_러너_게시글_응답 = PageResponse.from(페이징된_마이페이지_러너_게시글);
 
         return 페이징된_마이페이지_러너_게시글_응답;
+    }
+
+    public static SupporterRunnerPostResponse.Detail 지원한_서포터_응답(final Supporter 지원한_서포터,
+                                                                final int 서포터의_리뷰수,
+                                                                final String 지원한_서포터_어필_메시지,
+                                                                final List<String> 서포터_기술_태그_목록
+    ) {
+        return new SupporterRunnerPostResponse.Detail(
+                지원한_서포터.getId(),
+                지원한_서포터.getMember().getMemberName().getValue(),
+                지원한_서포터.getMember().getCompany().getValue(),
+                서포터의_리뷰수,
+                지원한_서포터.getMember().getImageUrl().getValue(),
+                지원한_서포터_어필_메시지,
+                서포터_기술_태그_목록
+        );
+    }
+
+    public static SupporterRunnerPostResponses.Detail 지원한_서포터_응답_목록_응답(final List<SupporterRunnerPostResponse.Detail> 지원한_서포터_응답_목록) {
+        return SupporterRunnerPostResponses.Detail.from(지원한_서포터_응답_목록);
     }
 
     public static class RunnerPostClientRequestBuilder {
@@ -119,7 +200,7 @@ public class RunnerPostAssuredSupport {
             return this;
         }
 
-        public RunnerPostClientRequestBuilder 러너_게시글_식별자값으로_서포터_러너_게시글을_조회한다(final Long 러너_게시글_식별자값) {
+        public RunnerPostClientRequestBuilder 러너_게시글_식별자값으로_지원한_서포터_목록을_조회한다(final Long 러너_게시글_식별자값) {
             response = AssuredSupport.get(
                     "/api/v1/posts/runner/{runnerPostId}/supporters",
                     accessToken,
@@ -266,7 +347,7 @@ public class RunnerPostAssuredSupport {
             );
         }
 
-        public void 서포터_러너_게시글_조회_성공을_검증한다(final SupporterRunnerPostResponses.Detail 전체_러너_게시글_페이징_응답) {
+        public void 지원한_서포터_목록_조회_성공을_검증한다(final SupporterRunnerPostResponses.Detail 전체_러너_게시글_페이징_응답) {
             final SupporterRunnerPostResponses.Detail actual = this.response.as(new TypeRef<>() {
             });
 
