ffec47161cdd6bcb0b68072feebd4f83c2ccc341
ditoo <shb03323@naver.com>
2023-09-10 16:57:02 +0900

    refactor: init.sql 칼럼 배치 순서 변경

--- backend/baton/src/main/resources/db/migration/V0__init.sql
+++ backend/baton/src/main/resources/db/migration/V0__init.sql
@@ -1,27 +1,27 @@
 create table member
 (
-    created_at datetime(6)  not null,
-    deleted_at datetime(6)  null,
     id         bigint auto_increment
         primary key,
-    updated_at datetime(6)  not null,
-    company    varchar(255) not null,
+    name       varchar(255) not null,
+    social_id  varchar(255) not null,
+    oauth_id   varchar(255) not null,
     github_url varchar(255) not null,
     image_url  varchar(255) not null,
-    name       varchar(255) not null,
-    oauth_id   varchar(255) not null,
-    social_id  varchar(255) not null
+    company    varchar(255) not null,
+    created_at datetime(6) not null,
+    updated_at datetime(6) not null,
+    deleted_at datetime(6) null
 );
 
 create table runner
 (
-    created_at   datetime(6)  not null,
-    deleted_at   datetime(6)  null,
     id           bigint auto_increment
         primary key,
-    member_id    bigint       not null,
-    updated_at   datetime(6)  not null,
     introduction varchar(255) not null,
+    created_at   datetime(6) not null,
+    updated_at   datetime(6) not null,
+    deleted_at   datetime(6) null,
+    member_id    bigint       not null,
     constraint UK_lk1nbff3heosfroj0h6joq0a7
         unique (member_id),
     constraint fk_runner_to_member
@@ -30,14 +30,14 @@ create table runner
 
 create table supporter
 (
-    review_count int default 0 null,
-    created_at   datetime(6)   not null,
-    deleted_at   datetime(6)   null,
     id           bigint auto_increment
         primary key,
-    member_id    bigint        not null,
-    updated_at   datetime(6)   not null,
-    introduction varchar(255)  not null,
+    review_count int default 0 null,
+    introduction varchar(255) not null,
+    created_at   datetime(6) not null,
+    updated_at   datetime(6) not null,
+    deleted_at   datetime(6) null,
+    member_id    bigint       not null,
     constraint UK_113bhk4mjpi996rm5uyj7eva9
         unique (member_id),
     constraint fk_supporter_to_member
@@ -46,19 +46,19 @@ create table supporter
 
 create table runner_post
 (
-    watch_count      int default 0                                          not null,
-    created_at       datetime(6)                                            not null,
-    deadline         datetime(6)                                            not null,
-    deleted_at       datetime(6)                                            null,
     id               bigint auto_increment
         primary key,
-    runner_id        bigint                                                 not null,
-    supporter_id     bigint                                                 null,
-    updated_at       datetime(6)                                            not null,
-    pull_request_url varchar(2083)                                          not null,
-    contents         text                                                   not null,
+    title            varchar(255)  not null,
+    contents         text          not null,
+    pull_request_url varchar(2083) not null,
+    deadline         datetime(6) not null,
+    watch_count      int default 0 not null,
     review_status    enum ('DONE', 'IN_PROGRESS', 'NOT_STARTED', 'OVERDUE') not null,
-    title            varchar(255)                                           not null,
+    created_at       datetime(6) not null,
+    updated_at       datetime(6) not null,
+    deleted_at       datetime(6) null,
+    runner_id        bigint        not null,
+    supporter_id     bigint null,
     constraint fk_runner_post_to_runner
         foreign key (runner_id) references runner (id),
     constraint fk_runner_post_to_supporter
@@ -67,16 +67,16 @@ create table runner_post
 
 create table supporter_feedback
 (
-    created_at     datetime(6)                   not null,
-    deleted_at     datetime(6)                   null,
     id             bigint auto_increment
         primary key,
-    runner_id      bigint                        not null,
-    runner_post_id bigint                        not null,
-    supporter_id   bigint                        not null,
-    updated_at     datetime(6)                   not null,
-    description    text                          null,
     review_type    enum ('BAD', 'GOOD', 'GREAT') not null,
+    description    text null,
+    created_at     datetime(6) not null,
+    updated_at     datetime(6) not null,
+    deleted_at     datetime(6) null,
+    supporter_id   bigint not null,
+    runner_id      bigint not null,
+    runner_post_id bigint not null,
     constraint UK_h2msmse508dwfs8uevwhq2xsn
         unique (runner_post_id),
     constraint fk_supporter_feed_back_to_runner
@@ -89,14 +89,14 @@ create table supporter_feedback
 
 create table supporter_runner_post
 (
-    created_at     datetime(6)  not null,
-    deleted_at     datetime(6)  null,
     id             bigint auto_increment
         primary key,
-    runner_post_id bigint       not null,
-    supporter_id   bigint       not null,
-    updated_at     datetime(6)  not null,
     message        varchar(255) not null,
+    created_at     datetime(6) not null,
+    updated_at     datetime(6) not null,
+    deleted_at     datetime(6) null,
+    supporter_id   bigint       not null,
+    runner_post_id bigint       not null,
     constraint fk_support_runner_post_to_runner_post
         foreign key (runner_post_id) references runner_post (id),
     constraint fk_support_runner_post_to_supporter
@@ -105,9 +105,10 @@ create table supporter_runner_post
 
 create table tag
 (
-    id   bigint auto_increment
+    id           bigint auto_increment
         primary key,
-    name varchar(255) not null,
+    name         varchar(255) not null,
+    reduced_name varchar(255) not null,
     constraint UK_1wdpsed5kna2y38hnbgrnhi5b
         unique (name)
 );
