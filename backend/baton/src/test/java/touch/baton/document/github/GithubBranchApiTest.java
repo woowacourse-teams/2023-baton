b88fa2d9c3fd9433e100779c85dfcd3b5b4ed0ff
ditoo <shb03323@naver.com>
2023-09-11 17:58:02 +0900

    test: docs 테스트 작성

---
+++ backend/baton/src/test/java/touch/baton/document/github/GithubBranchApiTest.java
@@ -0,0 +1,65 @@
+package touch.baton.document.github;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import touch.baton.config.RestdocsConfig;
+import touch.baton.domain.member.Member;
+import touch.baton.domain.member.controller.MemberBranchController;
+import touch.baton.domain.member.service.dto.GithubRepoNameRequest;
+import touch.baton.fixture.domain.MemberFixture;
+import touch.baton.infra.github.GithubBranchService;
+
+import java.util.Optional;
+
+import static org.apache.http.HttpHeaders.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.restdocs.headers.HeaderDocumentation.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(MemberBranchController.class)
+public class GithubBranchApiTest extends RestdocsConfig {
+
+    @MockBean
+    private GithubBranchService githubBranchService;
+
+    @BeforeEach
+    void setup() {
+        restdocsSetUp(new MemberBranchController(githubBranchService));
+    }
+
+    @DisplayName("깃허브 레포 브랜치 생성 API")
+    @Test
+    void createMemberBranch() throws Exception {
+        // given
+        final String socialId = "hongSile";
+        final Member member = MemberFixture.createWithSocialId(socialId);
+        final String accessToken = getAccessTokenBySocialId(socialId);
+        final GithubRepoNameRequest request = new GithubRepoNameRequest("drunken-ditoo");
+
+        // when
+        when(oauthMemberRepository.findBySocialId(any())).thenReturn(Optional.ofNullable(member));
+        doNothing().when(githubBranchService).createBranch(eq(socialId), anyString());
+
+        // then
+        mockMvc.perform(post("/api/v1/branch")
+                        .header(AUTHORIZATION, "Bearer " + accessToken)
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated())
+                .andExpect(redirectedUrl("/api/v1/profile/me"))
+                .andDo(restDocs.document(
+                        requestHeaders(headerWithName(AUTHORIZATION).description("Bearer Token"),
+                                headerWithName(CONTENT_TYPE).description(APPLICATION_JSON_VALUE)),
+                        responseHeaders(headerWithName(LOCATION).description("Redirect URI"))
+                ));
+    }
+}
