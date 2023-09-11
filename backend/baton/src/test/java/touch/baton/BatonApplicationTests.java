33ee0ea414e0ffca8a7372a9fffd4a6444d7a53c
ditoo <shb03323@naver.com>
2023-09-11 14:49:53 +0900

    refactor: 테스트 환경 변경

--- backend/baton/src/test/java/touch/baton/BatonApplicationTests.java
+++ backend/baton/src/test/java/touch/baton/BatonApplicationTests.java
@@ -2,9 +2,7 @@ package touch.baton;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
 @SpringBootTest
 class BatonApplicationTests {
 
--- backend/baton/src/test/java/touch/baton/config/RepositoryTestConfig.java
+++ backend/baton/src/test/java/touch/baton/config/RepositoryTestConfig.java
@@ -2,9 +2,7 @@ package touch.baton.config;
 
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
 @Import(JpaConfig.class)
 @DataJpaTest
 public abstract class RepositoryTestConfig {
