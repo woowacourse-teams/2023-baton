3ed1dee74c65eb57cd3fe58c585aa6bf9e981a46
gyeongza <gyeongza@naver.com>
2023-09-12 18:00:12 +0900

    style: 불필요한 console.log 삭제

--- frontend/webpack/webpack.common.js
+++ frontend/webpack/webpack.common.js
@@ -1,6 +1,5 @@
 const dotenv = require('dotenv');
 dotenv.config();
-console.log(process.env.REACT_APP_CHANNELTALK_KEY);
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
