bc810c8ca36136e7e35c6b448fa0ca0cd19def7d
Ethan <kim76744dev@gmail.com>
2023-09-14 21:10:51 +0900

    test: 날짜 테스트에서 local과 linux의 millisecond 자릿수가 다르던 문제 해결

--- backend/baton/src/test/java/touch/baton/domain/oauth/repository/RefreshTokenRepositoryTest.java
+++ backend/baton/src/test/java/touch/baton/domain/oauth/repository/RefreshTokenRepositoryTest.java
@@ -1,18 +1,19 @@
 package touch.baton.domain.oauth.repository;
 
 import jakarta.persistence.EntityManager;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import touch.baton.config.RepositoryTestConfig;
 import touch.baton.domain.member.Member;
-import touch.baton.domain.oauth.token.ExpireDate;
 import touch.baton.domain.oauth.token.RefreshToken;
 import touch.baton.domain.oauth.token.Token;
 import touch.baton.fixture.domain.MemberFixture;
 import touch.baton.fixture.domain.RefreshTokenFixture;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 import static java.time.LocalDateTime.now;
@@ -37,25 +38,26 @@ class RefreshTokenRepositoryTest extends RepositoryTestConfig {
         em.persist(ethan);
         em.persist(ditoo);
 
-        final LocalDateTime expireDate = now().plusDays(30);
+        final LocalDateTime expireDate = createExpireDate(now().plusDays(30));
+
         final Token ethanToken = token("ethan RefreshToken");
-        final RefreshToken actual = RefreshTokenFixture.create(ethan, ethanToken, expireDate(expireDate));
+        final RefreshToken expected = RefreshTokenFixture.create(ethan, ethanToken, expireDate(expireDate));
         final Token ditooToken = token("ditoo RefreshToken");
         final RefreshToken otherToken = RefreshTokenFixture.create(ditoo, ditooToken, expireDate(expireDate));
-        em.persist(actual);
+        em.persist(expected);
         em.persist(otherToken);
 
         em.flush();
         em.clear();
 
         // when
-        final Optional<RefreshToken> expected = refreshTokenRepository.findByToken(ethanToken);
+        final Optional<RefreshToken> actual = refreshTokenRepository.findByToken(ethanToken);
 
         // then
         assertSoftly(softly -> {
-            softly.assertThat(expected).isPresent();
-            softly.assertThat(expected.get().getToken().getValue()).isEqualTo(actual.getToken().getValue());
-            softly.assertThat(expected.get().getExpireDate().getValue()).isEqualTo(actual.getExpireDate().getValue());
+            softly.assertThat(actual).isPresent();
+            softly.assertThat(actual.get().getToken()).isEqualTo(expected.getToken());
+            softly.assertThat(actual.get().getExpireDate()).isEqualTo(expected.getExpireDate());
         } );
     }
 
@@ -68,23 +70,32 @@ class RefreshTokenRepositoryTest extends RepositoryTestConfig {
         em.persist(owner);
         em.persist(notOwner);
 
-        final ExpireDate expireDate = expireDate(now().plusDays(30));
-        final RefreshToken actual = RefreshTokenFixture.create(owner, token("ethan RefreshToken"), expireDate);
-        final RefreshToken differentRefreshToken = RefreshTokenFixture.create(notOwner, token("ditoo RefreshToken"), expireDate);
-        em.persist(actual);
+        final LocalDateTime expireDate = createExpireDate(now().plusDays(30));
+
+        final RefreshToken expected = RefreshTokenFixture.create(owner, token("ethan RefreshToken"), expireDate(expireDate));
+        final RefreshToken differentRefreshToken = RefreshTokenFixture.create(notOwner, token("ditoo RefreshToken"), expireDate(expireDate));
+        em.persist(expected);
         em.persist(differentRefreshToken);
 
         em.flush();
         em.clear();
 
         // when
-        final Optional<RefreshToken> expected = refreshTokenRepository.findByMember(owner);
+        final Optional<RefreshToken> actual = refreshTokenRepository.findByMember(owner);
 
         // then
         assertSoftly(softly -> {
-            softly.assertThat(expected).isPresent();
-            softly.assertThat(expected.get().getToken()).isEqualTo(actual.getToken());
-            softly.assertThat(expected.get().getExpireDate()).isEqualTo(actual.getExpireDate());
+            softly.assertThat(actual).isPresent();
+            softly.assertThat(actual.get().getToken()).isEqualTo(expected.getToken());
+            softly.assertThat(actual.get().getExpireDate()).isEqualTo(expected.getExpireDate());
         } );
+    }
+
+    @NotNull
+    private static LocalDateTime createExpireDate(final LocalDateTime expireDate) {
+        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
+        final String formattedExpireDate = expireDate.format(formatter);
+
+        return LocalDateTime.parse(formattedExpireDate, formatter);
     }
 }
