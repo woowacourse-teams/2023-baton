6fc25ad150fdb6ef884a01c2fabef62ff26b06dc
Ethan <kim76744dev@gmail.com>
2023-09-14 11:02:25 +0900

    test: 액세스 토큰 만료 테스트 구현

--- backend/baton/src/test/java/touch/baton/assure/oauth/OauthRefreshTokenAssuredTest.java
+++ backend/baton/src/test/java/touch/baton/assure/oauth/OauthRefreshTokenAssuredTest.java
@@ -25,11 +25,26 @@ class OauthRefreshTokenAssuredTest extends AssuredTestConfig {
 
     @Test
     void 만료된_JWT_로_본인_프로필_조회_요청을_보내면_오류가_발생한다() {
-        final String 기간_만료_엑세스_토큰 = jwtExpireEncoder.jwtToken(Map.of("socialId", MockAuthCodes.ethanAuthCode()));
+        OauthAssuredSupport
+                .클라이언트_요청()
+                .소셜_로그인을_위한_리다이렉트_URL을_요청한다(OauthType.GITHUB)
+
+                .서버_응답()
+                .소셜_로그인을_위한_리다이렉트_URL_요청_성공을_검증한다();
+
+        final Tokens 액세스_토큰과_리프레시_토큰 = OauthAssuredSupport
+                .클라이언트_요청()
+                .AuthCode를_통해_소셜_토큰을_발급_받은_후_사용자를_회원가입_한다(OauthType.GITHUB, MockAuthCodes.ethanAuthCode())
+
+                .서버_응답()
+                .AuthCode를_통해_소셜_토큰_발급_및_사용자_회원가입에_성공한다()
+                .액세스_토큰과_리프레시_토큰을_반환한다(MemberFixture.createEthan());
+
+        final String 기간_만료_액세스_토큰 = 기간_만료_액세스_토큰을_생성한다(액세스_토큰과_리프레시_토큰);
 
         OauthAssuredSupport
                 .클라이언트_요청()
-                .엑세스_토큰으로_로그인_한다(기간_만료_엑세스_토큰)
+                .엑세스_토큰으로_로그인_한다(기간_만료_액세스_토큰)
                 .기간이_만료된_엑세스_토큰으로_프로필_조회하려_한다()
 
                 .서버_응답()
