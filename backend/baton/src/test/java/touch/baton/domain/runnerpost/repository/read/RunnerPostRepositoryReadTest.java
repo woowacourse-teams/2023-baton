109190f721610e74231b16ef766299967921b5cf
ditoo <shb03323@naver.com>
2023-09-15 17:20:28 +0900

    test: 2차 정렬 테스트

--- backend/baton/src/test/java/touch/baton/domain/runnerpost/repository/read/RunnerPostRepositoryReadTest.java
+++ backend/baton/src/test/java/touch/baton/domain/runnerpost/repository/read/RunnerPostRepositoryReadTest.java
@@ -4,6 +4,9 @@ import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import touch.baton.config.RepositoryTestConfig;
 import touch.baton.domain.member.Member;
 import touch.baton.domain.member.repository.MemberRepository;
@@ -26,6 +29,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 import static touch.baton.domain.runnerpost.vo.ReviewStatus.NOT_STARTED;
 import static touch.baton.fixture.domain.RunnerPostTagsFixture.runnerPostTags;
 import static touch.baton.fixture.vo.CuriousContentsFixture.curiousContents;
@@ -54,16 +58,16 @@ class RunnerPostRepositoryReadTest extends RepositoryTestConfig {
     private RunnerPostTagRepository runnerPostTagRepository;
 
     @Autowired
-    private EntityManager entityManager;
+    private EntityManager em;
 
     @DisplayName("RunnerPost 식별자로 RunnerPostTag 목록을 조회할 때 Tag 가 있으면 조회된다.")
     @Test
     void findRunnerPostTagsById_exist() {
         // given
         final Member ditoo = MemberFixture.createDitoo();
-        entityManager.persist(ditoo);
+        em.persist(ditoo);
         final Runner runner = RunnerFixture.createRunner(ditoo);
-        entityManager.persist(runner);
+        em.persist(runner);
 
         final RunnerPost runnerPost = RunnerPostFixture.create(title("제 코드를 리뷰해주세요"),
                 implementedContents("제 코드의 내용은 이렇습니다."),
@@ -79,9 +83,9 @@ class RunnerPostRepositoryReadTest extends RepositoryTestConfig {
         runnerPostRepository.save(runnerPost);
 
         final Tag java = TagFixture.createJava();
-        entityManager.persist(java);
+        em.persist(java);
         final Tag spring = TagFixture.createSpring();
-        entityManager.persist(spring);
+        em.persist(spring);
         final RunnerPostTag javaRunnerPostTag = RunnerPostTagFixture.create(runnerPost, java);
         final RunnerPostTag springRunnerPostTag = RunnerPostTagFixture.create(runnerPost, spring);
 
@@ -99,9 +103,9 @@ class RunnerPostRepositoryReadTest extends RepositoryTestConfig {
     void findByRunnerId() {
         // given
         final Member ditoo = MemberFixture.createDitoo();
-        entityManager.persist(ditoo);
+        em.persist(ditoo);
         final Runner runner = RunnerFixture.createRunner(ditoo);
-        entityManager.persist(runner);
+        em.persist(runner);
 
         final RunnerPost runnerPost = RunnerPostFixture.create(title("제 코드를 리뷰해주세요"),
                 implementedContents("제 코드의 내용은 이렇습니다."),
@@ -121,5 +125,91 @@ class RunnerPostRepositoryReadTest extends RepositoryTestConfig {
 
         // then
         assertThat(actual).containsExactly(runnerPost);
+    }
+
+    @DisplayName("생성 시간이 같다면 다음 기준인 id 오름차순으로 게시글 페이징 조회한다")
+    @Test
+    void findByReviewStatus_when_createAt_is_same() {
+        // given
+        final Member ditoo = MemberFixture.createDitoo();
+        em.persist(ditoo);
+        final Runner runner = RunnerFixture.createRunner(ditoo);
+        em.persist(runner);
+        final Long runnerId = runner.getId();
+        final LocalDateTime createdAt = LocalDateTime.now();
+
+        em.createNativeQuery("""
+                        insert into runner_post (id, title, implemented_contents, curious_contents, postscript_contents, 
+                            pull_request_url, deadline, review_status, created_at, updated_at, runner_id)
+                        values (:id, :title, :implemented_contents, :curious_contents, :postscript_contents, 
+                            :pull_request_url, :deadline, :review_status, :created_at, :updated_at, :runner_id)
+                        """)
+                .setParameter("id", 1L)
+                .setParameter("title", "제목1")
+                .setParameter("implemented_contents", "구현 내용1")
+                .setParameter("curious_contents", "궁금한 내용1")
+                .setParameter("postscript_contents", "참고 사항1")
+                .setParameter("pull_request_url", "pr url1")
+                .setParameter("deadline", LocalDateTime.now().plusHours(100))
+                .setParameter("review_status", NOT_STARTED.name())
+                .setParameter("created_at", createdAt)
+                .setParameter("updated_at", createdAt)
+                .setParameter("runner_id", runnerId)
+                .executeUpdate();
+
+        em.createNativeQuery("""
+                        insert into runner_post (id, title, implemented_contents, curious_contents, postscript_contents, 
+                            pull_request_url, deadline, review_status, created_at, updated_at, runner_id)
+                        values (:id, :title, :implemented_contents, :curious_contents, :postscript_contents, 
+                            :pull_request_url, :deadline, :review_status, :created_at, :updated_at, :runner_id)
+                        """)
+                .setParameter("id", 3L)
+                .setParameter("title", "제목3")
+                .setParameter("implemented_contents", "구현 내용3")
+                .setParameter("curious_contents", "궁금한 내용3")
+                .setParameter("postscript_contents", "참고 사항3")
+                .setParameter("pull_request_url", "pr url3")
+                .setParameter("deadline", LocalDateTime.now().plusHours(100))
+                .setParameter("review_status", NOT_STARTED.name())
+                .setParameter("created_at", createdAt)
+                .setParameter("updated_at", createdAt)
+                .setParameter("runner_id", runnerId)
+                .executeUpdate();
+
+        em.createNativeQuery("""
+                        insert into runner_post (id, title, implemented_contents, curious_contents, postscript_contents, 
+                            pull_request_url, deadline, review_status, created_at, updated_at, runner_id)
+                        values (:id, :title, :implemented_contents, :curious_contents, :postscript_contents, 
+                            :pull_request_url, :deadline, :review_status, :created_at, :updated_at, :runner_id)
+                        """)
+                .setParameter("id", 2L)
+                .setParameter("title", "제목2")
+                .setParameter("implemented_contents", "구현 내용2")
+                .setParameter("curious_contents", "궁금한 내용2")
+                .setParameter("postscript_contents", "참고 사항2")
+                .setParameter("pull_request_url", "pr url2")
+                .setParameter("deadline", LocalDateTime.now().plusHours(100))
+                .setParameter("review_status", NOT_STARTED.name())
+                .setParameter("created_at", createdAt)
+                .setParameter("updated_at", createdAt)
+                .setParameter("runner_id", runnerId)
+                .executeUpdate();
+
+        // when
+        final PageRequest pageable = PageRequest.of(0, 10, Sort.by(
+                Sort.Order.desc("createdAt"),
+                Sort.Order.desc("id")
+        ));
+        final Page<RunnerPost> actual = runnerPostRepository.findByReviewStatus(pageable, NOT_STARTED);
+
+        // then
+        assertSoftly(softly -> {
+            softly.assertThat(actual.getTotalElements()).isEqualTo(3);
+
+            List<RunnerPost> contents = actual.getContent();
+            softly.assertThat(contents.get(0).getId()).isEqualTo(3L);
+            softly.assertThat(contents.get(1).getId()).isEqualTo(2L);
+            softly.assertThat(contents.get(2).getId()).isEqualTo(1L);
+        });
     }
 }
