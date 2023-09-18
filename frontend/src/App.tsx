27befff00eb974ec27d5442e88aafe05c4195efd
gyeongza <gyeongza@naver.com>
2023-09-18 18:15:29 +0900

    feat: 채널톡 복구

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,21 +2,21 @@ import React from 'react';
 import { styled } from 'styled-components';
 import { Outlet } from 'react-router-dom';
 import ToastProvider from './contexts/ToastContext';
-// import ChannelService from './ChannelService';
-// import { CHANNEL_SERVICE_KEY } from './constants';
+import ChannelService from './ChannelService';
+import { CHANNEL_SERVICE_KEY } from './constants';
 import { useLogin } from './hooks/useLogin';
 
 const App = () => {
   const { checkLoginToken } = useLogin();
 
   checkLoginToken();
-  // ChannelService.loadScript();
+  ChannelService.loadScript();
 
-  // if (CHANNEL_SERVICE_KEY) {
-  //   ChannelService.boot({
-  //     pluginKey: CHANNEL_SERVICE_KEY,
-  //   });
-  // }
+  if (CHANNEL_SERVICE_KEY) {
+    ChannelService.boot({
+      pluginKey: CHANNEL_SERVICE_KEY,
+    });
+  }
 
   return (
     <ToastProvider>
