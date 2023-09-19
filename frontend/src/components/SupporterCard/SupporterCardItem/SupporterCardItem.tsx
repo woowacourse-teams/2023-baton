5476a3cdeb8cf90082904f1510bf864013d4bf39
상규 <tkdrb@sang-gyuui-MacBookPro.local>
2023-09-19 17:25:45 +0900

    design: 서포터 선택 페이지 반응형 디자인 추가

--- frontend/src/components/SupporterCard/SupporterCardItem/SupporterCardItem.tsx
+++ frontend/src/components/SupporterCard/SupporterCardItem/SupporterCardItem.tsx
@@ -6,6 +6,7 @@ import { TOAST_COMPLETION_MESSAGE } from '@/constants/message';
 import { ToastContext } from '@/contexts/ToastContext';
 import { useFetch } from '@/hooks/useFetch';
 import { usePageRouter } from '@/hooks/usePageRouter';
+import useViewport from '@/hooks/useViewport';
 import { Candidate } from '@/types/supporterCandidate';
 import React, { useContext, useState } from 'react';
 import { useParams } from 'react-router-dom';
@@ -24,6 +25,8 @@ const SupporterCardItem = ({ supporter }: Props) => {
   const { showCompletionToast } = useContext(ToastContext);
 
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const { isMobile } = useViewport();
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -53,22 +56,31 @@ const SupporterCardItem = ({ supporter }: Props) => {
 
   return (
     <S.SupporterCardItemContainer>
+      <S.ReviewCountContainer>
+        완료된 리뷰
+        <S.ReviewCount> {supporter.reviewCount}</S.ReviewCount>
+      </S.ReviewCountContainer>
       <S.ProfileContainer>
-        <Avatar imageUrl={supporter.imageUrl} width={'80px'} height={'80px'} />
+        <Avatar imageUrl={supporter.imageUrl} width={isMobile ? '60px' : '80px'} height={isMobile ? '60px' : '80px'} />
         <S.InfoContainer>
           <S.Name>{supporter.name}</S.Name>
           <S.Company>{supporter.company}</S.Company>
-          <S.TechStackContainer>
-            {supporter.technicalTags.map((tag) => (
-              <TechLabel key={tag} tag={tag} />
-            ))}
-          </S.TechStackContainer>
+          {!isMobile && (
+            <S.TechStackContainer>
+              {supporter.technicalTags.map((tag) => (
+                <TechLabel key={tag} tag={tag} />
+              ))}
+            </S.TechStackContainer>
+          )}
         </S.InfoContainer>
-        <S.ReviewCountContainer>
-          완료된 리뷰
-          <S.ReviewCount> {supporter.reviewCount}</S.ReviewCount>
-        </S.ReviewCountContainer>
       </S.ProfileContainer>
+      {isMobile && (
+        <S.TechStackContainer>
+          {supporter.technicalTags.map((tag) => (
+            <TechLabel key={tag} tag={tag} />
+          ))}
+        </S.TechStackContainer>
+      )}
       <S.MessageContainer>
         📮 남긴 메시지
         <S.Message> {supporter.message}</S.Message>
@@ -99,12 +111,15 @@ const S = {
     display: flex;
     flex-direction: column;
 
-    height: 500px;
-
     padding: 30px 40px 40px 40px;
     border: 1px solid var(--gray-500);
     border-radius: 10px;
     box-shadow: 1px 4px 5px rgba(0, 0, 0, 0.2);
+
+    @media (max-width: 768px) {
+      height: 100%;
+      padding: 30px 23px;
+    }
   `,
 
   ProfileContainer: styled.div`
@@ -112,28 +127,48 @@ const S = {
     flex-wrap: wrap column;
     gap: 20px;
 
-    height: 100px;
+    height: 120px;
+    margin-bottom: 30px;
+
+    @media (max-width: 768px) {
+      height: 100%;
+      margin-bottom: 15px;
+    }
   `,
 
   InfoContainer: styled.div`
     display: flex;
     flex-direction: column;
+    justify-content: center;
     gap: 10px;
-
-    height: 100%;
   `,
 
   Name: styled.div`
     font-size: 20px;
     font-weight: 700;
+
+    @media (max-width: 768px) {
+      font-size: 16px;
+    }
   `,
 
   Company: styled.div`
     font-size: 18px;
+
+    @media (max-width: 768px) {
+      font-size: 14px;
+    }
   `,
 
   ReviewCountContainer: styled.div`
     margin-left: auto;
+    margin-bottom: 20px;
+
+    @media (max-width: 768px) {
+      margin-bottom: 15px;
+
+      font-size: 14px;
+    }
   `,
 
   ReviewCount: styled.span`
@@ -142,7 +177,12 @@ const S = {
 
   TechStackContainer: styled.div`
     display: flex;
+    flex-wrap: wrap;
     gap: 4px;
+
+    @media (max-width: 768px) {
+      margin-bottom: 45px;
+    }
   `,
 
   MessageContainer: styled.div`
@@ -151,6 +191,13 @@ const S = {
     gap: 15px;
 
     font-size: 18px;
+    margin-bottom: 35px;
+
+    @media (max-width: 768px) {
+      margin-bottom: 30px;
+
+      font-size: 14px;
+    }
   `,
 
   Message: styled.div`
@@ -168,6 +215,10 @@ const S = {
 
     &::-webkit-scrollbar {
       display: none;
+    }
+
+    @media (max-width: 768px) {
+      font-size: 14px;
     }
   `,
 
