42861699f617fc200fed1c409b250aee470cdab1
gyeongza <gyeongza@naver.com>
2023-09-19 15:04:45 +0900

    fix: favicon 설정 오류 수정

--- frontend/webpack/webpack.common.js
+++ frontend/webpack/webpack.common.js
@@ -21,7 +21,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '/public/index.html'),
+      template: path.resolve(__dirname, '../public/index.html'),
+      favicon: path.resolve(__dirname, '../public/favicon.png'),
     }),
     new webpack.ProvidePlugin({
       React: 'react',
