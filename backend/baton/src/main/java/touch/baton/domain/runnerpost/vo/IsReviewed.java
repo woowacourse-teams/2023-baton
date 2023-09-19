9c3193a921faaff9746c9c8b156762b83a9315cf
hyena <goatsfish@gmail.com>
2023-09-20 02:03:38 +0900

    feat: 러너 게시글의 서포터 피드백 유무 필드(컬럼) 구현

---
+++ backend/baton/src/main/java/touch/baton/domain/runnerpost/vo/IsReviewed.java
@@ -0,0 +1,33 @@
+package touch.baton.domain.runnerpost.vo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@Embeddable
+public class IsReviewed {
+
+    @ColumnDefault(value = "false")
+    @Column(name = "is_reviewed", nullable = false)
+    private boolean value = false;
+
+    private IsReviewed(final boolean value) {
+        this.value = value;
+    }
+
+    public static IsReviewed notReviewed() {
+        return new IsReviewed(false);
+    }
+
+    public static IsReviewed reviewed() {
+        return new IsReviewed(true);
+    }
+}
---
+++ backend/baton/src/test/java/touch/baton/domain/runnerpost/vo/IsReviewedTest.java
@@ -0,0 +1,21 @@
+package touch.baton.domain.runnerpost.vo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class IsReviewedTest {
+
+    @DisplayName("기본 생성의 default 값은 false 이다.")
+    @Test
+    void default_is_false() {
+        // given
+        final IsReviewed isReviewed = new IsReviewed();
+
+        // expect
+        final boolean actual = isReviewed.isValue();
+
+        assertThat(actual).isFalse();
+    }
+}
