ab058828abdae09a87e4a524e8d5439bd1b2e0f2
Ethan <kim76744dev@gmail.com>
2023-09-19 19:31:47 +0900

    refactor: tag 검색 API 에서 빈 값을 받으면 빈 리스트를 반환하도록 변경

--- backend/baton/src/main/java/touch/baton/domain/tag/controller/TagController.java
+++ backend/baton/src/main/java/touch/baton/domain/tag/controller/TagController.java
@@ -1,5 +1,6 @@
 package touch.baton.domain.tag.controller;
 
+import jakarta.annotation.Nullable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import touch.baton.domain.tag.controller.response.TagSearchResponse;
 import touch.baton.domain.tag.controller.response.TagSearchResponses;
 import touch.baton.domain.tag.service.TagService;
 
+import java.util.Collections;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -20,7 +22,11 @@ public class TagController {
     private final TagService tagService;
 
     @GetMapping("/search")
-    public ResponseEntity<TagSearchResponses.Detail> readTagsByTagName(@RequestParam String tagName) {
+    public ResponseEntity<TagSearchResponses.Detail> readTagsByTagName(@Nullable @RequestParam(required = false) final String tagName) {
+        if (tagName == null || tagName.isBlank()) {
+            return ResponseEntity.ok().body(TagSearchResponses.Detail.from(Collections.emptyList()));
+        }
+
         final List<TagSearchResponse.TagResponse> tagSearchResponses = tagService.readTagsByReducedName(tagName).stream()
                 .map(TagSearchResponse.TagResponse::from)
                 .toList();
