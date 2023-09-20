7889122edbdad75186462530953f9fd9b10de30f
Ethan <kim76744dev@gmail.com>
2023-09-20 12:10:47 +0900

    style: 예외 메세지 이름 변경

--- backend/baton/src/main/java/touch/baton/domain/feedback/service/FeedbackService.java
+++ backend/baton/src/main/java/touch/baton/domain/feedback/service/FeedbackService.java
@@ -33,7 +33,7 @@ public class FeedbackService {
                 .orElseThrow(() -> new FeedbackBusinessException("러너 게시글을 찾을 수 없습니다."));
 
         if (supporterFeedbackRepository.existsByRunnerPostIdAndSupporterId(foundRunnerPost.getId(), foundSupporter.getId())) {
-            throw new FeedbackBusinessException("서포터에대한 피드백을 작성했으면 추가적인 피드백을 남길 수 없습니다.");
+            throw new FeedbackBusinessException("서포터에 대한 피드백을 작성했으면 추가적인 피드백을 남길 수 없습니다.");
         }
         if (foundRunnerPost.isNotOwner(runner)) {
             throw new FeedbackBusinessException("리뷰 글을 작성한 주인만 글을 작성할 수 있습니다.");
