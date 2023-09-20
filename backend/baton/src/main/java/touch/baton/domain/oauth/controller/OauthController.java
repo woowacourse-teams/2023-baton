2db15f80fe93f743bcc06bd591f3c6aa8cea9eae
Ethan <kim76744dev@gmail.com>
2023-09-20 18:18:35 +0900

    refreshToken 과 accessToken 이 null 일 때 예외 추가 (#564)
    * feat: refreshToken 과 accessToken 이 null 일 때 예외 추가
    
    * feat: deploy 에 github branch 설정 추가
    
    * test: AssuredSupport post 에서 필요없는 Application json value 삭제
    
    * feat: CookieValue 에 Nullable 추가
    
    * Merbe deb/BE to feat/559
    
    * feat: max threads 100 으로 변경

--- backend/baton/src/main/java/touch/baton/domain/oauth/controller/OauthController.java
+++ backend/baton/src/main/java/touch/baton/domain/oauth/controller/OauthController.java
@@ -1,5 +1,6 @@
 package touch.baton.domain.oauth.controller;
 
+import jakarta.annotation.Nullable;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import touch.baton.domain.common.exception.ClientErrorCode;
+import touch.baton.domain.common.exception.ClientRequestException;
 import touch.baton.domain.oauth.AuthorizationHeader;
 import touch.baton.domain.oauth.OauthType;
 import touch.baton.domain.oauth.service.OauthService;
@@ -58,10 +61,17 @@ public class OauthController {
     }
 
     @PostMapping("/refresh")
-    public ResponseEntity<Void> refreshJwt(@CookieValue final String refreshToken,
+    public ResponseEntity<Void> refreshJwt(@Nullable @CookieValue(required = false) final String refreshToken,
                                            final HttpServletRequest request,
                                            final HttpServletResponse response
     ) {
+        if (request.getHeader(AUTHORIZATION) == null) {
+            throw new ClientRequestException(ClientErrorCode.OAUTH_AUTHORIZATION_VALUE_IS_NULL);
+        }
+        if (refreshToken == null || refreshToken.isBlank()) {
+            throw new ClientRequestException(ClientErrorCode.REFRESH_TOKEN_IS_NOT_NULL);
+        }
+
         final AuthorizationHeader authorizationHeader = new AuthorizationHeader(request.getHeader(AUTHORIZATION));
 
         final Tokens tokens = oauthService.reissueAccessToken(authorizationHeader, refreshToken);
