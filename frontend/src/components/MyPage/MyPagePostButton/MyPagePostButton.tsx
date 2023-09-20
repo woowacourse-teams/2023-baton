958210c230ab67ab8acde78abd5124a50b6b6510
gyeongza <gyeongza@naver.com>
2023-09-20 16:59:17 +0900

    design: 마이페이지 게시물 반응형 디자인 수정

--- frontend/src/components/MyPage/MyPagePostButton/MyPagePostButton.tsx
+++ frontend/src/components/MyPage/MyPagePostButton/MyPagePostButton.tsx
@@ -6,7 +6,8 @@ import { usePageRouter } from '@/hooks/usePageRouter';
 import useViewport from '@/hooks/useViewport';
 
 import { ReviewStatus } from '@/types/runnerPost';
-import React, { useContext, useMemo } from 'react';
+import React, { useContext } from 'react';
+import styled from 'styled-components';
 
 interface Props {
   runnerPostId: number;
@@ -26,15 +27,12 @@ const MyPagePostButton = ({ runnerPostId, reviewStatus, isRunner, supporterId }:
   const cancelReview = () => {
     patchRequestWithAuth(`/posts/runner/${runnerPostId}/cancelation`, async (response) => {
       showCompletionToast(TOAST_COMPLETION_MESSAGE.REVIEW_CANCEL);
-
-      setTimeout(window.location.reload, 2000);
     });
   };
 
   const finishReview = () => {
     patchRequestWithAuth(`/posts/runner/${runnerPostId}/done`, async (response) => {
       showCompletionToast(TOAST_COMPLETION_MESSAGE.REVIEW_COMPLETE);
-      setTimeout(window.location.reload, 2000);
     });
   };
 
@@ -68,42 +66,48 @@ const MyPagePostButton = ({ runnerPostId, reviewStatus, isRunner, supporterId }:
   switch (reviewStatus) {
     case 'NOT_STARTED':
       return (
-        <Button
-          fontSize={isMobile ? '14px' : ''}
-          colorTheme="WHITE"
-          fontWeight={700}
-          width={isMobile ? '100%' : '180px'}
-          height="40px"
-          onClick={isRunner ? handleClickSupportSelectButton : handleClickCancelReviewButton}
-        >
-          {isRunner ? '서포터 선택하기' : '리뷰 제안 취소'}
-        </Button>
+        <S.PostButtonWrapper>
+          <Button
+            fontSize={isMobile ? '14px' : ''}
+            colorTheme="WHITE"
+            fontWeight={700}
+            width={isMobile ? '100%' : '180px'}
+            height="40px"
+            onClick={isRunner ? handleClickSupportSelectButton : handleClickCancelReviewButton}
+          >
+            {isRunner ? '서포터 선택하기' : '리뷰 제안 취소'}
+          </Button>
+        </S.PostButtonWrapper>
       );
     case 'IN_PROGRESS':
       return isRunner ? null : (
-        <Button
-          colorTheme="WHITE"
-          fontWeight={700}
-          fontSize={isMobile ? '14px' : ''}
-          width={isMobile ? '100%' : '180px'}
-          height="40px"
-          onClick={handleClickFinishButton}
-        >
-          리뷰 완료
-        </Button>
+        <S.PostButtonWrapper>
+          <Button
+            colorTheme="WHITE"
+            fontWeight={700}
+            fontSize={isMobile ? '14px' : ''}
+            width={isMobile ? '100%' : '180px'}
+            height="40px"
+            onClick={handleClickFinishButton}
+          >
+            리뷰 완료
+          </Button>
+        </S.PostButtonWrapper>
       );
     case 'DONE':
       return isRunner ? (
-        <Button
-          colorTheme="WHITE"
-          fontWeight={700}
-          fontSize={isMobile ? '14px' : ''}
-          width={isMobile ? '100%' : '180px'}
-          height="40px"
-          onClick={handleClickSupportFeedbackButton}
-        >
-          후기 작성
-        </Button>
+        <S.PostButtonWrapper>
+          <Button
+            colorTheme="WHITE"
+            fontWeight={700}
+            fontSize={isMobile ? '14px' : ''}
+            width={isMobile ? '100%' : '180px'}
+            height="40px"
+            onClick={handleClickSupportFeedbackButton}
+          >
+            후기 작성
+          </Button>
+        </S.PostButtonWrapper>
       ) : null;
     default:
       return null;
@@ -111,3 +115,14 @@ const MyPagePostButton = ({ runnerPostId, reviewStatus, isRunner, supporterId }:
 };
 
 export default MyPagePostButton;
+
+const S = {
+  PostButtonWrapper: styled.div`
+    width: 180px;
+
+    @media (max-width: 768px) {
+      width: 100%;
+      margin-top: 18px;
+    }
+  `,
+};
