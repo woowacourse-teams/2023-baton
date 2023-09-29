de666786edddfce6f7051ecb48585746dd456afc
ditoo <shb03323@naver.com>
2023-09-29 14:32:47 +0900

    test: 서포터 id 로 러너 게시글 조회 레포지토리 테스트

--- backend/baton/src/test/java/touch/baton/domain/runnerpost/command/repository/read/RunnerPostPageRepositoryTest.java
+++ backend/baton/src/test/java/touch/baton/domain/runnerpost/command/repository/read/RunnerPostPageRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import touch.baton.config.RepositoryTestConfig;
 import touch.baton.domain.member.command.Runner;
+import touch.baton.domain.member.command.Supporter;
 import touch.baton.domain.runnerpost.command.RunnerPost;
 import touch.baton.domain.runnerpost.command.vo.IsReviewed;
 import touch.baton.domain.runnerpost.command.vo.ReviewStatus;
@@ -22,7 +23,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
-import static touch.baton.domain.runnerpost.command.vo.ReviewStatus.NOT_STARTED;
+import static touch.baton.domain.runnerpost.command.vo.ReviewStatus.*;
 import static touch.baton.fixture.vo.DeadlineFixture.deadline;
 
 class RunnerPostPageRepositoryTest extends RepositoryTestConfig {
@@ -34,7 +35,61 @@ class RunnerPostPageRepositoryTest extends RepositoryTestConfig {
         runnerPostPageRepository = new RunnerPostPageRepository(jpaQueryFactory);
     }
 
-    @DisplayName("리뷰 상태로 러너 게시글을 페이지 조회한다 (중간 페이지 조회)")
+    @DisplayName("러너 게시글을 페이징 조회한다 (중간 페이지 조회)")
+    @Test
+    void findByPageInfo() {
+        // given
+        final Runner runner = persistRunner(MemberFixture.createDitoo());
+        final ReviewStatus[] allReviewStatus = ReviewStatus.values();
+        final int persistSize = 30;
+        final List<Long> runnerPostIds = new ArrayList<>();
+        for (int i = 0; i < persistSize; i++) {
+            runnerPostIds.add(persistRunnerPost(runner, allReviewStatus[i % allReviewStatus.length]).getId());
+        }
+        final int lastIndex = persistSize - 1;
+        final Long previousLastId = runnerPostIds.get(lastIndex);
+        final int limit = 10;
+
+        // when
+        final List<RunnerPost> runnerPosts = runnerPostPageRepository.pageByReviewStatusAndTagReducedName(previousLastId, limit, null, null);
+        runnerPostIds.sort(Comparator.reverseOrder());
+        final List<Long> expected = runnerPostIds.subList(1, 1 + limit);
+
+        // then
+        assertSoftly(softly -> {
+           softly.assertThat(runnerPosts).hasSize(limit);
+           softly.assertThat(runnerPosts.stream().mapToLong(RunnerPost::getId))
+                   .isEqualTo(expected);
+        });
+    }
+
+    @DisplayName("러너 게시글을 페이징 조회한다 (첫 페이지 조회)")
+    @Test
+    void findLatestByLimit() {
+        // given
+        final Runner runner = persistRunner(MemberFixture.createDitoo());
+        final ReviewStatus[] allReviewStatus = ReviewStatus.values();
+        final int runnerPostCount = 10;
+        final List<Long> runnerPostIds = new ArrayList<>();
+        for (int i = 0; i < runnerPostCount; i++) {
+            runnerPostIds.add(persistRunnerPost(runner, allReviewStatus[i % allReviewStatus.length]).getId());
+        }
+        final int limit = 10;
+
+        // when
+        final List<RunnerPost> runnerPosts = runnerPostPageRepository.pageByReviewStatusAndTagReducedName(null, limit, null, null);
+        runnerPostIds.sort(Comparator.reverseOrder());
+        final List<Long> expected = runnerPostIds;
+
+        // then
+        assertSoftly(softly -> {
+            softly.assertThat(runnerPosts).hasSize(limit);
+            softly.assertThat(runnerPosts.stream().mapToLong(RunnerPost::getId))
+                    .isEqualTo(expected);
+        });
+    }
+
+    @DisplayName("리뷰 상태로 러너 게시글을 페이징 조회한다 (중간 페이지 조회)")
     @Test
     void findByPageInfoAndReviewStatus() {
         // given
@@ -56,13 +111,13 @@ class RunnerPostPageRepositoryTest extends RepositoryTestConfig {
 
         // then
         assertSoftly(softly -> {
-           softly.assertThat(runnerPosts).hasSize(limit);
-           softly.assertThat(runnerPosts.stream().mapToLong(RunnerPost::getId))
-                   .isEqualTo(expected);
+            softly.assertThat(runnerPosts).hasSize(limit);
+            softly.assertThat(runnerPosts.stream().mapToLong(RunnerPost::getId))
+                    .isEqualTo(expected);
         });
     }
 
-    @DisplayName("리뷰 상태로 러너 게시글을 페이지 조회한다 (첫 페이지 조회)")
+    @DisplayName("리뷰 상태로 러너 게시글을 페이징 조회한다 (첫 페이지 조회)")
     @Test
     void findLatestByLimitAndReviewStatus() {
         // given
@@ -88,7 +143,7 @@ class RunnerPostPageRepositoryTest extends RepositoryTestConfig {
         });
     }
 
-    @DisplayName("축약된 태그 이름과 리뷰 상태로 러너 게시글을 페이지 조회한다 (중간 페이지 조회)")
+    @DisplayName("축약된 태그 이름과 리뷰 상태로 러너 게시글을 페이징 조회한다 (중간 페이지 조회)")
     @Test
     void findByPageInfoAndReviewStatusAndTagReducedName() {
         // given
@@ -123,7 +178,7 @@ class RunnerPostPageRepositoryTest extends RepositoryTestConfig {
         });
     }
 
-    @DisplayName("축약된 태그 이름과 리뷰 상태로 러너 게시글을 페이지 조회한다 (첫 페이지 조회)")
+    @DisplayName("축약된 태그 이름과 리뷰 상태로 러너 게시글을 페이징 조회한다 (첫 페이지 조회)")
     @Test
     void findLatestByLimitAndTagNameAndReviewStatus() {
         // given
@@ -155,6 +210,161 @@ class RunnerPostPageRepositoryTest extends RepositoryTestConfig {
         });
     }
 
+    @DisplayName("서포터 외래키로 리뷰 상태가 NOT_STARTED 인 러너 게시글을 페이징 조회한다 (중간 페이지 조회)")
+    @Test
+    void findByPageInfoAndSupporterIdAndReviewStatus_NOT_STARTED() {
+        // given
+        final String tagName = "Javascript";
+        final Tag tag = persistTag(tagName);
+        final Runner runner = persistRunner(MemberFixture.createDitoo());
+        final Supporter supporter = persistSupporter(MemberFixture.createEthan());
+
+        final List<Long> runnerPostIds = new ArrayList<>();
+        final int persistSize = 30;
+        for (int i = 0; i < persistSize; i++) {
+            final RunnerPost runnerPost = persistRunnerPost(runner, NOT_STARTED);
+            persistRunnerPostTag(runnerPost, tag);
+            if (runnerPost.getId() % 2 == 0) {
+                persistApplicant(supporter, runnerPost);
+            }
+            runnerPostIds.add(runnerPost.getId());
+        }
+        final int lastIndex = persistSize - 1;
+        final Long previousLastId = runnerPostIds.get(lastIndex);
+        final int limit = 10;
+
+        // when
+        final List<RunnerPost> runnerPosts = runnerPostPageRepository.pageBySupporterIdAndReviewStatusNotStarted(previousLastId, limit, supporter.getId());
+        runnerPostIds.sort(Comparator.reverseOrder());
+        final List<Long> expected = runnerPostIds.stream()
+                .filter(id -> id % 2 == 0 && id < previousLastId)
+                .limit(limit)
+                .toList();
+
+        // then
+        assertSoftly(softly -> {
+            softly.assertThat(runnerPosts).hasSize(limit);
+            softly.assertThat(runnerPosts.stream().mapToLong(RunnerPost::getId))
+                    .isEqualTo(expected);
+        });
+    }
+
+    @DisplayName("서포터 외래키로 리뷰 상태가 NOT_STARTED 인 러너 게시글을 페이징 조회한다 (첫 페이지 조회)")
+    @Test
+    void findLatestByLimitAndSupporterIdAndReviewStatus_NOT_STARTED() {
+        // given
+        final String tagName = "Java";
+        final Tag tag = persistTag(tagName);
+        final Runner runner = persistRunner(MemberFixture.createDitoo());
+        final Supporter supporter = persistSupporter(MemberFixture.createEthan());
+
+        final List<Long> runnerPostIds = new ArrayList<>();
+        final int persistSize = 30;
+        for (int i = 0; i < persistSize; i++) {
+            final RunnerPost runnerPost = persistRunnerPost(runner, NOT_STARTED);
+            persistRunnerPostTag(runnerPost, tag);
+            if (runnerPost.getId() % 2 == 0) {
+                persistApplicant(supporter, runnerPost);
+            }
+            runnerPostIds.add(runnerPost.getId());
+        }
+        final int limit = 10;
+
+        // when
+        final List<RunnerPost> runnerPosts = runnerPostPageRepository.pageBySupporterIdAndReviewStatusNotStarted(null, limit, supporter.getId());
+        runnerPostIds.sort(Comparator.reverseOrder());
+        final List<Long> expected = runnerPostIds.stream()
+                .filter(id -> id % 2 == 0)
+                .limit(limit)
+                .toList();
+
+        // then
+        assertSoftly(softly -> {
+            softly.assertThat(runnerPosts).hasSize(limit);
+            softly.assertThat(runnerPosts.stream().mapToLong(RunnerPost::getId))
+                    .isEqualTo(expected);
+        });
+    }
+
+    @DisplayName("서포터 외래키로 리뷰 상태가 IN_PROGRESS 인 러너 게시글을 페이징 조회한다 (중간 페이지 조회)")
+    @Test
+    void findByPageInfoAndSupporterIdAndReviewStatus_IN_PROGRESS() {
+        // given
+        final String tagName = "Javascript";
+        final Tag tag = persistTag(tagName);
+        final Runner runner = persistRunner(MemberFixture.createDitoo());
+        final Supporter supporter = persistSupporter(MemberFixture.createEthan());
+
+        final List<Long> runnerPostIds = new ArrayList<>();
+        final int persistSize = 30;
+        for (int i = 0; i < persistSize; i++) {
+            final RunnerPost runnerPost = persistRunnerPost(runner, NOT_STARTED);
+            persistRunnerPostTag(runnerPost, tag);
+            if (runnerPost.getId() % 2 == 0) {
+                persistApplicant(supporter, runnerPost);
+                runnerPost.assignSupporter(supporter);
+            }
+            runnerPostIds.add(runnerPost.getId());
+        }
+        final int lastIndex = persistSize - 1;
+        final Long previousLastId = runnerPostIds.get(lastIndex);
+        final int limit = 10;
+
+        // when
+        final List<RunnerPost> runnerPosts = runnerPostPageRepository.pageBySupporterIdAndReviewStatus(previousLastId, limit, supporter.getId(), IN_PROGRESS);
+        runnerPostIds.sort(Comparator.reverseOrder());
+        final List<Long> expected = runnerPostIds.stream()
+                .filter(id -> id % 2 == 0 && id < previousLastId)
+                .limit(limit)
+                .toList();
+
+        // then
+        assertSoftly(softly -> {
+            softly.assertThat(runnerPosts).hasSize(limit);
+            softly.assertThat(runnerPosts.stream().mapToLong(RunnerPost::getId))
+                    .isEqualTo(expected);
+        });
+    }
+
+    @DisplayName("서포터 외래키로 리뷰 상태가 DONE 인 러너 게시글을 페이징 조회한다 (첫 페이지 조회)")
+    @Test
+    void findLatestByLimitAndSupporterIdAndReviewStatus_DONE() {
+        // given
+        final String tagName = "Java";
+        final Tag tag = persistTag(tagName);
+        final Runner runner = persistRunner(MemberFixture.createDitoo());
+        final Supporter supporter = persistSupporter(MemberFixture.createEthan());
+
+        final List<Long> runnerPostIds = new ArrayList<>();
+        final int persistSize = 30;
+        for (int i = 0; i < persistSize; i++) {
+            final RunnerPost runnerPost = persistRunnerPost(runner, NOT_STARTED);
+            persistRunnerPostTag(runnerPost, tag);
+            if (runnerPost.getId() % 2 == 0) {
+                persistApplicant(supporter, runnerPost);
+                runnerPost.assignSupporter(supporter);
+                runnerPost.finishReview();
+            }
+            runnerPostIds.add(runnerPost.getId());
+        }
+        final int limit = 10;
+
+        // when
+        final List<RunnerPost> runnerPosts = runnerPostPageRepository.pageBySupporterIdAndReviewStatus(null, limit, supporter.getId(), DONE);
+        runnerPostIds.sort(Comparator.reverseOrder());
+        final List<Long> expected = runnerPostIds.stream()
+                .filter(id -> id % 2 == 0)
+                .limit(limit)
+                .toList();
+
+        // then
+        assertSoftly(softly -> {
+            softly.assertThat(runnerPosts).hasSize(limit);
+            softly.assertThat(runnerPosts.stream().mapToLong(RunnerPost::getId))
+                    .isEqualTo(expected);
+        });
+    }
+
     @DisplayName("RunnerPost 목록으로 RunnerPostTag 목록을 조회한다.")
     @Test
     void findRunnerPostTagsByRunnerPosts() {
@@ -164,6 +374,7 @@ class RunnerPostPageRepositoryTest extends RepositoryTestConfig {
 
         final List<RunnerPost> runnerPosts = new ArrayList<>();
         final List<RunnerPostTag> expected = new ArrayList<>();
+
         for (int i = 0; i < 10; i++) {
             final Runner runner = persistRunner(MemberFixture.createDitoo());
             final RunnerPost runnerPost = persistRunnerPost(runner);
