30c25cbb0c3a0d44c5c449062084cc9384ccbdc5
상규 <tkdrb@sang-gyuui-MacBookPro.local>
2023-10-01 23:31:34 +0900

    feat: useMyGithubUrl react-query hook 추가

---
+++ frontend/src/hooks/useMyGithubUrl.ts
@@ -0,0 +1,29 @@
+import { getMyRunnerProfile } from '@/apis/apis';
+import { ERROR_TITLE } from '@/constants/message';
+import { ToastContext } from '@/contexts/ToastContext';
+import { APIError } from '@/types/error';
+import { GetRunnerProfileResponse } from '@/types/profile';
+import { useQuery } from '@tanstack/react-query';
+import { useContext, useEffect } from 'react';
+
+export const useMyGithubUrl = () => {
+  const { showErrorToast } = useContext(ToastContext);
+
+  const queryResult = useQuery<GetRunnerProfileResponse, APIError, string>({
+    queryKey: ['myRunnerProfile'],
+    queryFn: getMyRunnerProfile,
+    select: (data) => {
+      return data.githubUrl;
+    },
+  });
+
+  useEffect(() => {
+    if (queryResult.error) {
+      showErrorToast({ title: ERROR_TITLE.NETWORK, description: '내 깃허브 정보를 불러오지 못했어요' });
+    }
+  }, [queryResult.error]);
+
+  return {
+    data: queryResult.data as NonNullable<typeof queryResult.data>,
+  };
+};
