5c4394033c926f26b988ef1d13c1a01719c4c908
상규 <tkdrb@sang-gyuui-MacBookPro.local>
2023-10-01 23:28:25 +0900

    feat: useRunnerPostCreation react-query hook 추가 및 queryClient 분할

---
+++ frontend/src/hooks/useRunnerPostCreation.ts
@@ -0,0 +1,34 @@
+import { useContext } from 'react';
+import { usePageRouter } from './usePageRouter';
+import { ToastContext } from '@/contexts/ToastContext';
+import { useMutation } from '@tanstack/react-query';
+import { APIError } from '@/types/error';
+import { CreateRunnerPostRequest } from '@/types/runnerPost';
+import { postRunnerPostCreation } from '@/apis/apis';
+import { ERROR_TITLE, TOAST_COMPLETION_MESSAGE, TOAST_ERROR_MESSAGE } from '@/constants/message';
+import { queryClient } from './query/queryClient';
+
+export const useRunnerPostCreation = () => {
+  const { showErrorToast, showCompletionToast } = useContext(ToastContext);
+  const { goToMainPage } = usePageRouter();
+
+  const queryResult = useMutation<void, APIError, CreateRunnerPostRequest>({
+    mutationFn: async (formData: CreateRunnerPostRequest) => {
+      postRunnerPostCreation(formData);
+    },
+
+    onSuccess: () => {
+      showCompletionToast(TOAST_COMPLETION_MESSAGE.CREATE_POST);
+      queryClient.invalidateQueries({ queryKey: ['myRunnerPost', 'runnerPost'] });
+      goToMainPage();
+    },
+
+    onError: () => {
+      showErrorToast({ title: ERROR_TITLE.REQUEST, description: '게시물 등록에 실패했어요' });
+    },
+
+    retry: 1,
+  });
+
+  return queryResult;
+};
