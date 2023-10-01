ec244461ad1fc2415aef8340844d6682f6d8e19b
상규 <tkdrb@sang-gyuui-MacBookPro.local>
2023-10-02 01:04:52 +0900

    feat: useReviewCancelation react-query hook 추가

---
+++ frontend/src/hooks/query/useReviewCancelation.ts
@@ -0,0 +1,28 @@
+import { useContext } from 'react';
+import { ToastContext } from '@/contexts/ToastContext';
+import { useMutation } from '@tanstack/react-query';
+import { APIError } from '@/types/error';
+import { patchReviewCancelation } from '@/apis/apis';
+import { ERROR_TITLE, TOAST_COMPLETION_MESSAGE } from '@/constants/message';
+import { queryClient } from './queryClient';
+
+export const useReviewCancelation = () => {
+  const { showErrorToast, showCompletionToast } = useContext(ToastContext);
+
+  const queryResult = useMutation<void, APIError, number>({
+    mutationFn: async (runnerPostId: number) => patchReviewCancelation(runnerPostId),
+
+    onSuccess: () => {
+      showCompletionToast(TOAST_COMPLETION_MESSAGE.REVIEW_CANCEL);
+      queryClient.invalidateQueries({ queryKey: ['mySupporterPost', 'NOT_STARTED'] });
+    },
+
+    onError: () => {
+      showErrorToast({ title: ERROR_TITLE.REQUEST, description: '리뷰 취소 요청이 실패했어요' });
+    },
+
+    retry: 1,
+  });
+
+  return queryResult;
+};
