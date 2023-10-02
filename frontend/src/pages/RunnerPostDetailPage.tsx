5dfa8937b957a71fb804302769783da77dfd408a
상규 <tkdrb@sang-gyuui-MacBookPro.local>
2023-10-02 15:17:22 +0900

    RunnerPostDetailPage react qeury 적용

--- frontend/src/pages/RunnerPostDetailPage.tsx
+++ frontend/src/pages/RunnerPostDetailPage.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { styled } from 'styled-components';
 import { useParams } from 'react-router-dom';
 import { usePageRouter } from '@/hooks/usePageRouter';
@@ -10,61 +10,38 @@ import applicantIcon from '@/assets/applicant-icon.svg';
 import Button from '@/components/common/Button/Button';
 import { REVIEW_STATUS_LABEL_TEXT } from '@/constants/index';
 import Label from '@/components/common/Label/Label';
-import { GetDetailedRunnerPostResponse } from '@/types/runnerPost';
 import ConfirmModal from '@/components/ConfirmModal/ConfirmModal';
 import githubIcon from '@/assets/github-icon.svg';
-import { ERROR_DESCRIPTION, ERROR_TITLE, TOAST_COMPLETION_MESSAGE, TOAST_ERROR_MESSAGE } from '@/constants/message';
+import { ERROR_DESCRIPTION, ERROR_TITLE } from '@/constants/message';
 import { ToastContext } from '@/contexts/ToastContext';
 import SendMessageModal from '@/components/SendMessageModal/SendMessageModal';
 import { validateMessage } from '@/utils/validate';
 import useViewport from '@/hooks/useViewport';
-import { useFetch } from '@/hooks/useFetch';
 import { useLogin } from '@/hooks/useLogin';
 import GuideContents from '@/components/GuideContents/GuideContents';
+import { useRunnerPostDetail } from '@/hooks/query/useRunnerPostDetail';
+import { useRunnerPostDelete } from '@/hooks/query/useRunnerPostDelete';
+import { useReviewSuggestion } from '@/hooks/query/useReviewSuggestion';
 
 const RunnerPostPage = () => {
-  const { goToMainPage, goBack, goToRunnerProfilePage, goToMyPage, goToLoginPage } = usePageRouter();
+  const { goBack, goToRunnerProfilePage, goToLoginPage } = usePageRouter();
 
-  const { runnerPostId } = useParams();
-  const { getRequest, getRequestWithAuth, postRequestWithAuth, deleteRequestWithAuth } = useFetch();
+  const { paramRunnerPostId } = useParams();
+  const runnerPostId = Number(paramRunnerPostId);
+
   const { isLogin } = useLogin();
 
   const { isMobile } = useViewport();
 
-  const { showErrorToast, showCompletionToast } = useContext(ToastContext);
+  const { showErrorToast } = useContext(ToastContext);
 
-  const [runnerPost, setRunnerPost] = useState<GetDetailedRunnerPostResponse | null>(null);
   const [isConfirmModalOpen, setIsConfirmModalOpen] = useState<boolean>(false);
   const [isMessageModalOpen, setIsMessageModalOpen] = useState<boolean>(false);
   const [message, setMessage] = useState<string>('');
 
-  useEffect(() => {
-    getRunnerPost();
-  }, []);
-
-  const getRunnerPost = () => {
-    if (isLogin) {
-      getRequestWithAuth(`/posts/runner/${runnerPostId}`, async (response) => {
-        const data: GetDetailedRunnerPostResponse = await response.json();
-        setRunnerPost(data);
-      });
-
-      return;
-    }
-
-    getRequest(`/posts/runner/${runnerPostId}`, async (response) => {
-      const data: GetDetailedRunnerPostResponse = await response.json();
-      setRunnerPost(data);
-    });
-  };
-
-  const deleteRunnerPost = () => {
-    deleteRequestWithAuth(`/posts/runner/${runnerPostId}`, () => {
-      showCompletionToast(TOAST_COMPLETION_MESSAGE.DELETE);
-
-      goToMainPage();
-    });
-  };
+  const { data: runnerPostDetail } = useRunnerPostDetail(runnerPostId, isLogin);
+  const { mutate: deleteRunnerPost } = useRunnerPostDelete();
+  const { mutate: suggestReview, isPending: isPendingSuggestion } = useReviewSuggestion();
 
   const sendMessage = () => {
     try {
@@ -76,15 +53,7 @@ const RunnerPostPage = () => {
       return;
     }
 
-    postRequestWithAuth(
-      `/posts/runner/${runnerPostId}/application`,
-      () => {
-        showCompletionToast(TOAST_COMPLETION_MESSAGE.SUBMISSION);
-
-        goToMyPage();
-      },
-      JSON.stringify({ message: message }),
-    );
+    suggestReview({ runnerPostId, message });
   };
 
   const handleChangeMessage = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -94,7 +63,7 @@ const RunnerPostPage = () => {
   const handleClickDeleteButton = () => {
     setIsConfirmModalOpen(false);
 
-    deleteRunnerPost();
+    deleteRunnerPost(runnerPostId);
   };
 
   const openConfirmModal = () => {
@@ -121,117 +90,119 @@ const RunnerPostPage = () => {
   };
 
   const viewProfile = () => {
-    if (runnerPost) {
-      goToRunnerProfilePage(runnerPost?.runnerProfile.runnerId);
-    }
+    const runnerId = runnerPostDetail.runnerProfile.runnerId;
+    goToRunnerProfilePage(runnerId);
   };
 
   return (
     <Layout>
       <S.RunnerPostContainer>
-        {runnerPost && (
-          <S.PostContainer>
-            <S.PostHeaderContainer>
-              <Label
-                width="131px"
-                height="33px"
-                fontSize="16px"
-                colorTheme={
-                  runnerPost.reviewStatus === 'DONE'
-                    ? 'GRAY'
-                    : runnerPost.reviewStatus === 'IN_PROGRESS'
-                    ? 'RED'
-                    : 'WHITE'
+        <S.PostContainer>
+          <S.PostHeaderContainer>
+            <Label
+              width="131px"
+              height="33px"
+              fontSize="16px"
+              colorTheme={
+                runnerPostDetail.reviewStatus === 'DONE'
+                  ? 'GRAY'
+                  : runnerPostDetail.reviewStatus === 'IN_PROGRESS'
+                  ? 'RED'
+                  : 'WHITE'
+              }
+            >
+              {REVIEW_STATUS_LABEL_TEXT[runnerPostDetail.reviewStatus]}
+            </Label>
+            <S.PostDeadlineContainer>
+              <S.PostDeadline>{runnerPostDetail.deadline.replace('T', ' ')} 까지</S.PostDeadline>
+              <S.EditLinkContainer
+                $isOwner={
+                  runnerPostDetail.isOwner &&
+                  runnerPostDetail.reviewStatus !== 'IN_PROGRESS' &&
+                  runnerPostDetail.reviewStatus !== 'DONE'
                 }
               >
-                {REVIEW_STATUS_LABEL_TEXT[runnerPost.reviewStatus]}
-              </Label>
-              <S.PostDeadlineContainer>
-                <S.PostDeadline>{runnerPost.deadline.replace('T', ' ')} 까지</S.PostDeadline>
-                <S.EditLinkContainer
-                  $isOwner={
-                    runnerPost.isOwner &&
-                    runnerPost.reviewStatus !== 'IN_PROGRESS' &&
-                    runnerPost.reviewStatus !== 'DONE'
-                  }
+                <S.EditLink onClick={openConfirmModal}>삭제</S.EditLink>
+              </S.EditLinkContainer>
+            </S.PostDeadlineContainer>
+            <S.PostTitleContainer>
+              <S.PostTitle>{runnerPostDetail.title}</S.PostTitle>
+              <S.InformationContainer>
+                <S.statisticsContainer>
+                  <S.statisticsImage src={eyeIcon} />
+                  <S.statisticsText>{runnerPostDetail.watchedCount}</S.statisticsText>
+                  <S.statisticsImage src={applicantIcon} />
+                  <S.statisticsText>{runnerPostDetail.applicantCount}</S.statisticsText>
+                </S.statisticsContainer>
+              </S.InformationContainer>
+            </S.PostTitleContainer>
+          </S.PostHeaderContainer>
+          <S.PostBodyContainer>
+            <S.GuideContentsContainer>
+              <GuideContents title="무엇을 구현하였나요?" contents={runnerPostDetail.implementedContents} />
+              <GuideContents title="아쉬운 점이나 궁금한 점이 있나요?" contents={runnerPostDetail.curiousContents} />
+              {runnerPostDetail.postscriptContents && (
+                <GuideContents
+                  title="서포터에게 하고싶은 말이 있나요?"
+                  contents={runnerPostDetail.postscriptContents}
+                />
+              )}
+            </S.GuideContentsContainer>
+            <S.BottomContentContainer>
+              <S.LeftSideContainer>
+                <S.ProfileContainer onClick={viewProfile}>
+                  <Avatar
+                    width={isMobile ? '40px' : '60px'}
+                    height={isMobile ? '40px' : '60px'}
+                    imageUrl={runnerPostDetail.runnerProfile.imageUrl}
+                  />
+                  <S.Profile>
+                    <S.Name>{runnerPostDetail.runnerProfile.name}</S.Name>
+                    <S.Job>{runnerPostDetail.runnerProfile.company}</S.Job>
+                  </S.Profile>
+                </S.ProfileContainer>
+                <PostTagList tags={runnerPostDetail.tags} />
+              </S.LeftSideContainer>
+              <S.RightSideContainer>
+                <Button
+                  colorTheme="BLACK"
+                  width={isMobile ? '130px' : '150px'}
+                  fontSize={isMobile ? '14px' : ''}
+                  fontWeight={700}
                 >
-                  <S.EditLink onClick={openConfirmModal}>삭제</S.EditLink>
-                </S.EditLinkContainer>
-              </S.PostDeadlineContainer>
-              <S.PostTitleContainer>
-                <S.PostTitle>{runnerPost.title}</S.PostTitle>
-                <S.InformationContainer>
-                  <S.statisticsContainer>
-                    <S.statisticsImage src={eyeIcon} />
-                    <S.statisticsText>{runnerPost.watchedCount}</S.statisticsText>
-                    <S.statisticsImage src={applicantIcon} />
-                    <S.statisticsText>{runnerPost.applicantCount}</S.statisticsText>
-                  </S.statisticsContainer>
-                </S.InformationContainer>
-              </S.PostTitleContainer>
-            </S.PostHeaderContainer>
-            <S.PostBodyContainer>
-              <S.GuideContentsContainer>
-                <GuideContents title="무엇을 구현하였나요?" contents={runnerPost.implementedContents} />
-                <GuideContents title="아쉬운 점이나 궁금한 점이 있나요?" contents={runnerPost.curiousContents} />
-                {runnerPost.postscriptContents && (
-                  <GuideContents title="서포터에게 하고싶은 말이 있나요?" contents={runnerPost.postscriptContents} />
-                )}
-              </S.GuideContentsContainer>
-              <S.BottomContentContainer>
-                <S.LeftSideContainer>
-                  <S.ProfileContainer onClick={viewProfile}>
-                    <Avatar
-                      width={isMobile ? '40px' : '60px'}
-                      height={isMobile ? '40px' : '60px'}
-                      imageUrl={runnerPost.runnerProfile.imageUrl}
-                    />
-                    <S.Profile>
-                      <S.Name>{runnerPost.runnerProfile.name}</S.Name>
-                      <S.Job>{runnerPost.runnerProfile.company}</S.Job>
-                    </S.Profile>
-                  </S.ProfileContainer>
-                  <PostTagList tags={runnerPost.tags} />
-                </S.LeftSideContainer>
-                <S.RightSideContainer>
+                  <S.Anchor href={runnerPostDetail.pullRequestUrl} target="_blank">
+                    <img src={githubIcon} />
+                    <S.GoToGitHub>코드 보러가기</S.GoToGitHub>
+                  </S.Anchor>
+                </Button>
+                {runnerPostDetail.isOwner ||
+                runnerPostDetail.isApplied ||
+                runnerPostDetail.reviewStatus !== 'NOT_STARTED' ? null : (
                   <Button
-                    colorTheme="BLACK"
+                    colorTheme="WHITE"
                     width={isMobile ? '130px' : '150px'}
                     fontSize={isMobile ? '14px' : ''}
                     fontWeight={700}
+                    onClick={openMessageModal}
                   >
-                    <S.Anchor href={runnerPost.pullRequestUrl} target="_blank">
-                      <img src={githubIcon} />
-                      <S.GoToGitHub>코드 보러가기</S.GoToGitHub>
-                    </S.Anchor>
+                    리뷰 제안하기
                   </Button>
-                  {runnerPost.isOwner || runnerPost.isApplied || runnerPost.reviewStatus !== 'NOT_STARTED' ? null : (
-                    <Button
-                      colorTheme="WHITE"
-                      width={isMobile ? '130px' : '150px'}
-                      fontSize={isMobile ? '14px' : ''}
-                      fontWeight={700}
-                      onClick={openMessageModal}
-                    >
-                      리뷰 제안하기
-                    </Button>
-                  )}
-                </S.RightSideContainer>
-              </S.BottomContentContainer>
-            </S.PostBodyContainer>
-            <S.PostFooterContainer>
-              <Button
-                width={isMobile ? '60px' : '180px'}
-                colorTheme="GRAY"
-                fontSize={isMobile ? '14px' : ''}
-                fontWeight={700}
-                onClick={goBack}
-              >
-                목록
-              </Button>
-            </S.PostFooterContainer>
-          </S.PostContainer>
-        )}
+                )}
+              </S.RightSideContainer>
+            </S.BottomContentContainer>
+          </S.PostBodyContainer>
+          <S.PostFooterContainer>
+            <Button
+              width={isMobile ? '60px' : '180px'}
+              colorTheme="GRAY"
+              fontSize={isMobile ? '14px' : ''}
+              fontWeight={700}
+              onClick={goBack}
+            >
+              목록
+            </Button>
+          </S.PostFooterContainer>
+        </S.PostContainer>
       </S.RunnerPostContainer>
 
       {isConfirmModalOpen && (
