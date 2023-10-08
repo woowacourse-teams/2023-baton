f32248615b47c27b1c408b5961f99314b78cc667
guridaek <xylitol79@gmail.com>
2023-10-08 17:10:29 +0900

    feat: 인증 관련 오류 발생시 로그아웃

--- frontend/src/apis/error.ts
+++ frontend/src/apis/error.ts
@@ -1,8 +1,6 @@
-import { ACCESS_TOKEN_LOCAL_STORAGE_KEY } from '@/constants';
 import { ERROR_DESCRIPTION, ERROR_TITLE } from '@/constants/message';
 import { APIError, CustomApiError } from '@/types/error';
-
-const removeAccessToken = () => localStorage.removeItem(ACCESS_TOKEN_LOCAL_STORAGE_KEY);
+import { logout } from './auth';
 
 export const validateResponse = async (response: Response) => {
   if (response.ok) return;
@@ -11,14 +9,9 @@ export const validateResponse = async (response: Response) => {
     const apiError: APIError = await response.json();
 
     if (apiError.errorCode.includes('JW') || apiError.errorCode.includes('OA')) {
-      removeAccessToken();
-      window.location.reload();
+      logout();
 
-      const authErrorCode = ['JW007', 'JW008', 'JW009', 'JW010'];
-      throw new CustomApiError(
-        ERROR_TITLE.NO_PERMISSION,
-        authErrorCode.includes(apiError.errorCode) ? ERROR_DESCRIPTION.TOKEN_EXPIRATION : ERROR_DESCRIPTION.NO_TOKEN,
-      );
+      throw new CustomApiError(ERROR_TITLE.NO_PERMISSION, ERROR_DESCRIPTION.NO_TOKEN);
     }
   } catch (error) {
     throw new CustomApiError(ERROR_TITLE.REQUEST, `${response.status}: ` + ERROR_DESCRIPTION.UNEXPECTED);
