0a42e75197725cbcf43c7153a476d726eba4a190
gyeongza <gyeongza@naver.com>
2023-10-10 18:22:14 +0900

    feat: 구글 애널리틱스 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,20 @@ import LoadingPage from './pages/LoadingPage';
 import ModalProvider from './contexts/ModalContext';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './hooks/query/queryClient';
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
+
+const gaTrackingId = process.env.REACT_APP_GA_TRACKING_ID;
+if (gaTrackingId) {
+  ReactGA.initialize(gaTrackingId);
+}
+
+const history = createBrowserHistory();
+history.listen((response: { location: { pathname: string } }) => {
+  console.log(response.location.pathname);
+  ReactGA.set({ page: response.location.pathname });
+  ReactGA.pageview(response.location.pathname);
+});
 
 const App = () => {
   ChannelService.loadScript();
