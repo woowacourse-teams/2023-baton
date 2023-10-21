ec7cef56366902e6b368c61c6753b7ff836f3af6
상규 <tkdrb@sang-gyuui-MacBookPro.local>
2023-10-21 17:29:47 +0900

    feat: 리뷰 제안 취소, 완료 기능에 optimistic update 적용

--- frontend/src/hooks/query/useReviewCancelation.ts
+++ frontend/src/hooks/query/useReviewCancelation.ts
@@ -1,10 +1,11 @@
 import { useContext } from 'react';
 import { ToastContext } from '@/contexts/ToastContext';
-import { useMutation } from '@tanstack/react-query';
+import { InfiniteData, useMutation } from '@tanstack/react-query';
 import { APIError } from '@/types/error';
 import { patchReviewCancelation } from '@/apis/apis';
 import { ERROR_TITLE, TOAST_COMPLETION_MESSAGE } from '@/constants/message';
 import { queryClient } from './queryClient';
+import { GetMyPagePostResponse } from '@/types/myPage';
 
 export const useReviewCancelation = () => {
   const { showErrorToast, showCompletionToast } = useContext(ToastContext);
@@ -12,13 +13,35 @@ export const useReviewCancelation = () => {
   const queryResult = useMutation<void, APIError, number>({
     mutationFn: (runnerPostId: number) => patchReviewCancelation(runnerPostId),
 
+    onMutate: async (deletedPostId: number) => {
+      await queryClient.cancelQueries({ queryKey: ['mySupporterPost', 'NOT_STARTED'] });
+
+      const previous = queryClient.getQueryData(['mySupporterPost', 'NOT_STARTED']);
+
+      queryClient.setQueryData<InfiniteData<GetMyPagePostResponse>>(['mySupporterPost', 'NOT_STARTED'], (oldData) => {
+        if (!oldData) return undefined;
+
+        const newData = oldData.pages.map((page) => {
+          return { ...page, data: page.data.filter((item) => item.runnerPostId !== deletedPostId) };
+        });
+
+        return {
+          ...oldData,
+          pages: newData,
+        };
+      });
+
+      return { previous };
+    },
+
     onSuccess: () => {
       showCompletionToast(TOAST_COMPLETION_MESSAGE.REVIEW_CANCEL);
-      queryClient.refetchQueries({ queryKey: ['mySupporterPost', 'NOT_STARTED'] });
     },
 
     onError: () => {
       showErrorToast({ title: ERROR_TITLE.REQUEST, description: '리뷰 취소 요청이 실패했어요' });
+
+      queryClient.invalidateQueries({ queryKey: ['mySupporterPost', 'NOT_STARTED'] });
     },
   });
 
