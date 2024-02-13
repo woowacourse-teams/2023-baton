1e3baa6f8be8c71512a527a64c7fb6905b345f4b
gyeongza <gyeongza@naver.com>
2024-02-13 13:10:44 +0900

    refactor: Flex 컴포넌트 gap prop 추가

--- frontend/src/components/common/Flex/Flex.tsx
+++ frontend/src/components/common/Flex/Flex.tsx
@@ -5,13 +5,15 @@ interface FlexProps {
   align?: CSSProperties['alignItems'];
   justify?: CSSProperties['justifyContent'];
   direction?: CSSProperties['flexDirection'];
+  gap?: CSSProperties['gap'];
 }
 
-const Flex = styled.div<FlexProps>(({ align, justify, direction }) => ({
+const Flex = styled.div<FlexProps>(({ align, justify, direction, gap }) => ({
   display: 'flex',
   alignItems: align,
   justifyContent: justify,
   flexDirection: direction,
+  gap: `${gap}px`,
 }));
 
 export default Flex;
