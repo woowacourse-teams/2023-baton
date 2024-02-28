9511bcfc34518a1888996558f4a62aad6d4ac65b
박경현 <62369936+gyeongza@users.noreply.github.com>
2024-02-28 15:28:29 +0900

    프론트엔드 빌드 속도 개선 및 번들 사이즈 최적화 (#733)
    refactor: esbuild-loader로 변경 및 sideEffect 옵션 설정

--- frontend/webpack/webpack.common.js
+++ frontend/webpack/webpack.common.js
@@ -10,7 +10,7 @@ module.exports = {
     rules: [
       {
         test: /\.(ts|tsx|js|jsx)$/,
-        use: 'babel-loader',
+        use: 'esbuild-loader',
         exclude: /node_modules/,
       },
       {
